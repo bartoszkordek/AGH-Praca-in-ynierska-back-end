2700e4c4e65700df884a8a39bd3a23cc0f7cd124
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-24 16:45:22 +0100

    Add initial user-service

---
+++ user-service/src/test/java/com/healthy/gym/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthy.gym.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

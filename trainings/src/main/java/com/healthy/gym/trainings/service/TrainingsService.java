516aace617fc2312dc8a0c85097f72fa0875b9db
bartoszkordek <bartosz.kordek10@gmail.com>
2021-03-31 00:07:20 +0200

    signed client controller init

--- trainings/src/main/java/com/healthy/gym/trainings/service/TrainingsService.java
+++ trainings/src/main/java/com/healthy/gym/trainings/service/TrainingsService.java
@@ -5,6 +5,8 @@ import com.healthy.gym.trainings.db.GroupTrainingsRepository;
 import com.healthy.gym.trainings.db.TestRepository;
 import com.healthy.gym.trainings.model.GroupTrainingModel;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -25,5 +27,9 @@ public class TrainingsService {
 
     public List<GroupTrainingModel> getGroupTrainings() throws JsonProcessingException {
         return groupTrainingsRepository.getGroupTrainings();
+    }
+
+    public void enrollToGroupTraining(Object trainingId, String clientId){
+
     }
 }

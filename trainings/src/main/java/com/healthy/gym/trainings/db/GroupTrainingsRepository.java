69c8c54d34f0dd42fa63ebd6d34e5830f3d7346e
bartoszkordek <bartosz.kordek10@gmail.com>
2021-04-01 01:23:49 +0200

    group training enrollment updates

--- trainings/src/main/java/com/healthy/gym/trainings/db/GroupTrainingsRepository.java
+++ trainings/src/main/java/com/healthy/gym/trainings/db/GroupTrainingsRepository.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.healthy.gym.trainings.model.GroupTrainingModel;
 import com.mongodb.client.*;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Repository;
 import org.bson.Document;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -48,5 +52,42 @@ public class GroupTrainingsRepository {
             cursor.close();
         }
         return groupTrainingsList;
+    }
+
+    public boolean isAbilityToGroupTrainingEnrollment(String trainingId){
+
+        mongoClient = MongoClients.create();
+        mdb = mongoClient.getDatabase(environment.getProperty("microservice.db.name"));
+        MongoCollection collection = mdb.getCollection(environment.getProperty("microservice.db.collection"));
+
+        ObjectId oTrainingId = new ObjectId(trainingId.toString());
+        Document idDoc = new Document("_id", oTrainingId);
+
+        if(collection.find(idDoc).cursor().hasNext() == false) return false;
+
+        MongoCursor<Document> cursor = collection.find(idDoc).iterator();
+        List<String> participants = new ArrayList<>();
+        try {
+            boolean stop = false;
+            while(cursor.hasNext() && cursor!= null && stop == false) {
+                participants = (List<String>) cursor.next().get("participants");
+                stop = true;
+            }
+        } finally {
+            cursor.close();
+        }
+        Document gtParticipantsLimit = new Document("$gt", participants.size());
+        Document limit = new Document("limit", gtParticipantsLimit);
+
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");
+        Date now = new Date();
+        String todayDateFormatted = sdfDate.format(now);
+
+        Document gtToday = new Document("$gt", todayDateFormatted);
+        Document gtTodayDate = new Document("date", gtToday);
+
+        Document match = new Document("$match", new Document("$and", Arrays.asList(idDoc,gtTodayDate,limit)));
+        List<Document> pipeline = Arrays.asList(match);
+        return collection.aggregate(pipeline).cursor().hasNext();
     }
 }
--- trainings/src/main/java/com/healthy/gym/trainings/service/TrainingsService.java
+++ trainings/src/main/java/com/healthy/gym/trainings/service/TrainingsService.java
@@ -3,10 +3,9 @@ package com.healthy.gym.trainings.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.healthy.gym.trainings.db.GroupTrainingsRepository;
 import com.healthy.gym.trainings.db.TestRepository;
+import com.healthy.gym.trainings.exception.TrainingEnrollmentException;
 import com.healthy.gym.trainings.model.GroupTrainingModel;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -29,7 +28,7 @@ public class TrainingsService {
         return groupTrainingsRepository.getGroupTrainings();
     }
 
-    public void enrollToGroupTraining(Object trainingId, String clientId){
-
+    public void enrollToGroupTraining(String trainingId, String clientId) throws TrainingEnrollmentException {
+        if(trainingId.length() != 24 || !groupTrainingsRepository.isAbilityToGroupTrainingEnrollment(trainingId)) throw new TrainingEnrollmentException("Cannot enroll to this training");
     }
 }

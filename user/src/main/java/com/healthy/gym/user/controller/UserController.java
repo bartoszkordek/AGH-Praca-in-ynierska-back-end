552cc80c36ceef3c3813fee8a2b6762b13f33658
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-07 15:45:41 +0200

    Change package structure.

--- user/src/main/java/com/healthy/gym/user/controller/UserController.java
+++ user/src/main/java/com/healthy/gym/user/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.healthy.gym.user.ui.controller;
+package com.healthy.gym.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;

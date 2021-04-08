3ce5878b47d63041b15ea1bb7d8c55ecca0dfec9
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-08 21:53:31 +0200

    Add repository and service layer of UserService

--- user/src/main/java/com/healthy/gym/user/service/UserService.java
+++ user/src/main/java/com/healthy/gym/user/service/UserService.java
@@ -1,5 +1,10 @@
 package com.healthy.gym.user.service;
 
-public interface UserService {
+import com.healthy.gym.user.shared.UserDTO;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
     String status();
+    UserDTO createUser(UserDTO userDetails);
+    UserDTO getUserDetailsByEmail(String email);
 }

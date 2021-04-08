a1a3f34270b2831d397c4b9d2ed6463fd9a0a9c5
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-08 18:29:35 +0200

    Add UserEntity

--- user/src/main/java/com/healthy/gym/user/data/entity/UserEntity.java
+++ user/src/main/java/com/healthy/gym/user/data/entity/UserEntity.java
@@ -1,4 +1,102 @@
 package com.healthy.gym.user.data.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 60, name = "name")
+    private String name;
+
+    @Column(nullable = false, length = 60, name = "surname")
+    private String surname;
+
+    @Column(nullable = false, length = 120, name = "email", unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 60, name = "phone_number")
+    private String phoneNumber;
+
+    @Column(nullable = false, name = "password")
+    private String encryptedPassword;
+
+    @Column(nullable = false, name = "userID", unique = true, updatable = false)
+    private String userId;
+
+    public UserEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", encryptedPassword='" + encryptedPassword + '\'' +
+                ", userId='" + userId + '\'' +
+                '}';
+    }
 }

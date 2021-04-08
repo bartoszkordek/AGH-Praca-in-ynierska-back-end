7574cf86aef51c0898b428df375913e84e4ed3e2
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-08 13:21:28 +0200

    Add initial UserController and UserControllerTest

--- user/src/main/java/com/healthy/gym/user/controller/UserController.java
+++ user/src/main/java/com/healthy/gym/user/controller/UserController.java
@@ -1,11 +1,19 @@
 package com.healthy.gym.user.controller;
 
+import com.healthy.gym.user.pojo.request.CreateUserRequest;
+import com.healthy.gym.user.pojo.response.CreateUserResponse;
 import com.healthy.gym.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -14,12 +22,50 @@ public class UserController {
     private UserService userService;
 
     @Autowired
-    public UserController(UserService userService){
-        this.userService=userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CreateUserResponse> createUser(
+            @Valid @RequestBody CreateUserRequest createUserRequest, BindingResult bindingResult
+    ) {
+        if (bindingResult.hasFieldErrors()) {
+            CreateUserResponse invalidCreateUserResponse = handleInvalidRegistration(bindingResult);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(invalidCreateUserResponse);
+        }
+
+        CreateUserResponse response = handleValidRegistration(createUserRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    private CreateUserResponse handleInvalidRegistration(BindingResult bindingResult) {
+        Map<String, String> errors = new HashMap<>();
+        CreateUserResponse response =
+                new CreateUserResponse(false, "Rejestracja zakończona niepowodzeniem.", errors);
+
+        bindingResult.getAllErrors().forEach(error -> {
+            if (error instanceof FieldError) {
+                FieldError fieldError = (FieldError) error;
+                String fieldName = fieldError.getField();
+                String errorMessage = fieldError.getDefaultMessage();
+                errors.put(fieldName, errorMessage);
+            } else {
+                String errorMessage = error.getDefaultMessage();
+                errors.put("matchingPassword", errorMessage);
+            }
+        });
+
+        return response;
+    }
+
+    private CreateUserResponse handleValidRegistration(CreateUserRequest createUserRequest) {
+        return new CreateUserResponse(true, "Użytkownik został zarejestrowany.", new HashMap<>());
     }
 
     @GetMapping("/status")
-    public @ResponseBody String status(){
+    public @ResponseBody
+    String status() {
         return userService.status();
     }
 }
--- user/src/test/java/com/healthy/gym/user/controller/UserControllerTest.java
+++ user/src/test/java/com/healthy/gym/user/controller/UserControllerTest.java
@@ -1,20 +1,25 @@
 package com.healthy.gym.user.controller;
 
 import com.healthy.gym.user.service.UserService;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.net.URI;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.ResultMatcher.matchAll;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
@@ -26,7 +31,7 @@ class UserControllerTest {
     private UserService userService;
 
     @Test
-    void userControllerShouldReturnDefaultStatusMessage() throws Exception {
+    void shouldReturnDefaultStatusMessage() throws Exception {
         URI uri = new URI("/users/status");
 
         when(userService.status()).thenReturn("OK");
@@ -37,4 +42,33 @@ class UserControllerTest {
                 .andExpect(content().string(containsString("OK")));
     }
 
+    @Test
+    void shouldRejectUserRegistrationWhenInvalidRequestWasSend() throws Exception {
+        URI uri = new URI("/users/");
+        String newUser = "{" +
+                "\"name\": \"G\",\n" +
+                "\"surname\": \"Z\",\n" +
+                "\"email\": \"g.zacharskiwp.pl\",\n" +
+                "\"phone\": \"665 763 6831\",\n" +
+                "\"password\": \"testtest123\",\n" +
+                "\"matchingPassword\": \"testtest1234\"" +
+                "}";
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post(uri)
+                .content(newUser)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request).andExpect(
+                matchAll(
+                        status().isBadRequest(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        jsonPath("$.success").value("false"),
+                        jsonPath("$.message").value("Rejestracja zakończona niepowodzeniem."),
+                        jsonPath("$.errors").isMap(),
+                        jsonPath("$.errors.matchingPassword").value("Podane hasła się nie zgadzają"),
+                        jsonPath("$.errors.surname", Is.is("Pole nazwisko powinno mieć od 2 do 60 znaków."))
+                )
+        );
+    }
 }

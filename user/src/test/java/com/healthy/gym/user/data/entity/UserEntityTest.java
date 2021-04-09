864e8dd01f25c01aae85cd07a6ed40b59752b2e0
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-09 02:10:58 +0200

    Add tests of DAO layer and UserEntity

---
+++ user/src/test/java/com/healthy/gym/user/data/entity/UserEntityTest.java
@@ -0,0 +1,13 @@
+package com.healthy.gym.user.data.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserEntityTest {
+
+    @Test
+    void name() {
+        assertTrue(true);
+    }
+}
--- user/src/test/java/com/healthy/gym/user/data/repository/UserDAOTest.java
+++ user/src/test/java/com/healthy/gym/user/data/repository/UserDAOTest.java
@@ -1,13 +1,52 @@
 package com.healthy.gym.user.data.repository;
 
+import com.healthy.gym.user.data.entity.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 class UserDAOTest {
 
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private UserDAO userDAO;
+
+    private UserEntity userEntity;
+
+    @BeforeEach
+    void setUp() {
+        userEntity = new UserEntity();
+        userEntity.setUserId(UUID.randomUUID().toString());
+        userEntity.setName("John");
+        userEntity.setSurname("Smith");
+        userEntity.setEmail("john.smith@gmail.com");
+        userEntity.setEncryptedPassword("password");
+        userEntity.setPhoneNumber("546 324 324");
+
+        testEntityManager.persist(userEntity);
+        testEntityManager.flush();
+    }
+
     @Test
-    void name() {
-        assertTrue(true);
+    void shouldReturnUserWhenFindByName() {
+        String email = "john.smith@gmail.com";
+        UserEntity responseEntity = userDAO.findByEmail(email);
+        assertThat(responseEntity.getEmail()).isEqualTo(email);
+    }
+
+    @Test
+    void shouldNotReturnUserWhenFindByName() {
+        String email = "john1.smith@gmail.com";
+        UserEntity responseEntity = userDAO.findByEmail(email);
+        assertThat(responseEntity).isNull();
     }
 }

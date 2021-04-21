ecce146267cdd96d9a347cb798d2e8cb82833b8a
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-21 22:34:45 +0200

    Add CreateUserResponse tests

--- user/src/main/java/com/healthy/gym/user/pojo/response/CreateUserResponse.java
+++ user/src/main/java/com/healthy/gym/user/pojo/response/CreateUserResponse.java
@@ -2,8 +2,8 @@ package com.healthy.gym.user.pojo.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Objects;
 
 public class CreateUserResponse {
 
@@ -12,7 +12,6 @@ public class CreateUserResponse {
     private boolean success;
     private String message;
     private Map<String, String> errors;
-    private String timestamp;
 
     public CreateUserResponse() {
     }
@@ -26,8 +25,7 @@ public class CreateUserResponse {
         this.success = success;
         this.message = message;
         this.errors = errors;
-        this.timestamp = LocalDateTime.now().toString();
-        this.userId=userId;
+        this.userId = userId;
     }
 
     public boolean isSuccess() {
@@ -54,15 +52,27 @@ public class CreateUserResponse {
         this.errors = errors;
     }
 
-    public String getTimestamp() {
-        return timestamp;
-    }
-
     public String getUserId() {
         return userId;
     }
 
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CreateUserResponse that = (CreateUserResponse) o;
+        return success == that.success
+                && Objects.equals(userId, that.userId)
+                && Objects.equals(message, that.message)
+                && Objects.equals(errors, that.errors);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, success, message, errors);
     }
 }

c33141063b10f62e2dbc91b67d1a59098feaac93
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-21 23:57:01 +0200

    Add UserDAOIntegrationTest

--- user/src/main/java/com/healthy/gym/user/data/entity/UserEntity.java
+++ user/src/main/java/com/healthy/gym/user/data/entity/UserEntity.java
@@ -33,6 +33,15 @@ public class UserEntity {
         //empty constructor required by JPA
     }
 
+    public UserEntity(String name, String surname, String email, String phoneNumber, String encryptedPassword, String userId) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.encryptedPassword = encryptedPassword;
+        this.userId = userId;
+    }
+
     public Long getId() {
         return id;
     }

a400e09405d1e411e52aa3bc1da8dbeb10b61ecf
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-22 19:44:15 +0200

    Add UserApplicationTests

--- user/src/test/java/com/healthy/gym/user/UserApplicationTests.java
+++ user/src/test/java/com/healthy/gym/user/UserApplicationTests.java
@@ -1,17 +1,28 @@
 package com.healthy.gym.user;
 
+import com.healthy.gym.user.component.Translator;
 import com.healthy.gym.user.controller.UserController;
+import com.healthy.gym.user.data.repository.UserDAO;
+import com.healthy.gym.user.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class UserApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Autowired
     private UserController userController;
@@ -20,6 +31,15 @@ class UserApplicationTests {
     @WithMockUser
     void contextLoads() {
         assertThat(userController).isNotNull();
+    }
+
+    @Test
+    void shouldReturnComponentsInTheCurrentScope(){
+        assertThat(applicationContext.getBean(UserController.class)).isNotNull();
+        assertThat(applicationContext.getBean(Translator.class)).isNotNull();
+        assertThat(applicationContext.getBean(UserService.class)).isNotNull();
+        assertThat(applicationContext.getBean(UserDAO.class)).isNotNull();
+        assertThat(applicationContext.getBean(BCryptPasswordEncoder.class)).isNotNull();
     }
 
 }

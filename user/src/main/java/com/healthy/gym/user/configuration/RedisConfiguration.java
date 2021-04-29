bca02840d9fba29da19f3b70345a071867d92738
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-29 15:54:52 +0200

    Add RedisConfiguration

---
+++ user/src/main/java/com/healthy/gym/user/configuration/RedisConfiguration.java
@@ -0,0 +1,20 @@
+package com.healthy.gym.user.configuration;
+
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@EnableConfigurationProperties(RedisProperties.class)
+public class RedisConfiguration {
+
+	@Bean
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		return template;
+	}
+}

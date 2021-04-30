5e9c3f4bf94c8706c7c5cad4f522ef41234c932c
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-30 18:25:58 +0200

    Add Translator interface and refactor

--- user/src/main/java/com/healthy/gym/user/component/Translator.java
+++ user/src/main/java/com/healthy/gym/user/component/Translator.java
@@ -1,28 +1,8 @@
 package com.healthy.gym.user.component;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.stereotype.Component;
+public interface Translator {
 
-import java.util.Locale;
+    String toLocale(String message, Object[] args);
 
-@Component
-public class Translator {
-
-    private final MessageSource messageSource;
-
-    @Autowired
-    public Translator(MessageSource messageSource) {
-        this.messageSource = messageSource;
-    }
-
-    public String toLocale(String message, Object[] args) {
-        Locale locale = LocaleContextHolder.getLocale();
-        return messageSource.getMessage(message, args, locale);
-    }
-
-    public String toLocale(String message) {
-        return toLocale(message, null);
-    }
+    String toLocale(String message);
 }

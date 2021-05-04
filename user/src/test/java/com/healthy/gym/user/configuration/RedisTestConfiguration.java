c4079cfae608f02363840a43087e99ef2da3fd94
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-04 19:18:51 +0200

    Add RedisTestConfiguration and refactor RedisConfiguration

--- user/src/test/java/com/healthy/gym/user/configuration/RedisTestConfiguration.java
+++ user/src/test/java/com/healthy/gym/user/configuration/RedisTestConfiguration.java
@@ -2,48 +2,41 @@ package com.healthy.gym.user.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
-@Configuration
-public class RedisConfiguration {
+@TestConfiguration
+public class RedisTestConfiguration {
 
     private final Environment environment;
     private final RedisProperties redisProperties;
 
     @Autowired
-    public RedisConfiguration(Environment environment) {
+    public RedisTestConfiguration(Environment environment) {
         this.environment = environment;
         this.redisProperties = new RedisProperties();
     }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        return template;
-    }
-
-    @Bean
-    public LettuceConnectionFactory connectionFactory() {
+    @Primary
+    public LettuceConnectionFactory testConnectionFactory() {
         var redisStandaloneConfiguration = new RedisStandaloneConfiguration();
 
-        redisStandaloneConfiguration.setPort(redisProperties.getPort());
+        redisStandaloneConfiguration.setDatabase(redisProperties.getDatabase());
         redisStandaloneConfiguration.setHostName(redisProperties.getHost());
-        redisStandaloneConfiguration.setDatabase(getRedisDatabase());
+        redisStandaloneConfiguration.setPort(getRedisTestPort());
         redisStandaloneConfiguration.setPassword(getRedisPassword());
 
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
-    private int getRedisDatabase() {
-        String database = environment.getRequiredProperty("spring.redis.database");
-        return Integer.parseInt(database);
+    private int getRedisTestPort() {
+        String port = environment.getRequiredProperty("spring.redis.port");
+        return Integer.parseInt(port);
     }
 
     private String getRedisPassword() {

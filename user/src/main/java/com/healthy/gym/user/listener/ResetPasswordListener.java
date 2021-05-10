e923faac0b487298045d0d1deb68504528e825a3
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-10 19:14:46 +0200

    Add OnResetPasswordEvent and ResetPasswordListener

---
+++ user/src/main/java/com/healthy/gym/user/listener/ResetPasswordListener.java
@@ -0,0 +1,54 @@
+package com.healthy.gym.user.listener;
+
+import com.healthy.gym.user.component.MailMessageManager;
+import com.healthy.gym.user.data.entity.ResetPasswordToken;
+import com.healthy.gym.user.data.entity.UserEntity;
+import com.healthy.gym.user.data.repository.ResetPasswordTokenDAO;
+import com.healthy.gym.user.events.OnResetPasswordEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResetPasswordListener {
+
+    private final JavaMailSender javaMailSender;
+    private final ResetPasswordTokenDAO resetPasswordTokenDAO;
+    private final MailMessageManager mailMessageManager;
+
+    public ResetPasswordListener(
+            JavaMailSender javaMailSender,
+            ResetPasswordTokenDAO resetPasswordTokenDAO,
+            MailMessageManager mailMessageManager
+    ) {
+        this.javaMailSender = javaMailSender;
+        this.resetPasswordTokenDAO = resetPasswordTokenDAO;
+        this.mailMessageManager = mailMessageManager;
+    }
+
+    @Async
+    @EventListener
+    public void sendEmailToResetPassword(OnResetPasswordEvent event) {
+
+        ResetPasswordToken resetPasswordToken = resetPasswordTokenDAO.save(event.getResetPasswordToken());
+
+        UserEntity user = resetPasswordToken.getUserEntity();
+        String token = resetPasswordToken.getToken();
+
+        if (user == null || token == null)
+            throw new IllegalStateException();
+
+        String recipientAddress = user.getEmail();
+        String subject = mailMessageManager.getResetPasswordMessageSubject();
+        String text = mailMessageManager.getResetPasswordMessageText(resetPasswordToken);
+
+        SimpleMailMessage confirmationEmail = new SimpleMailMessage();
+        confirmationEmail.setTo(recipientAddress);
+        confirmationEmail.setSubject(subject);
+        confirmationEmail.setText(text);
+
+        javaMailSender.send(confirmationEmail);
+    }
+}

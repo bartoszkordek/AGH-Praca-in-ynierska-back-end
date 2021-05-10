3f3b6b602293ef4be930eacc7cfd7d3e405bc615
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-10 14:20:33 +0200

    Add reset.password.exception messages.

--- user/src/test/java/com/healthy/gym/user/configuration/tests/Messages.java
+++ user/src/test/java/com/healthy/gym/user/configuration/tests/Messages.java
@@ -33,8 +33,10 @@ public class Messages {
                 {"registration.confirmation.token.error", "Wystąpił błąd podczas rejestracji. Skontaktuj się z administratorem."},
                 {"reset.password.error", "Wystąpił błąd resetowania hasła. Skontaktuj się z administratorem."},
                 {"reset.password", "Link do zmiany hasła został wysłany na podany adres email."},
-                {"reset.password.account.status.exception",
-                        "Nie możesz resetować hasła. Twoje konto nie zostało jeszcze aktywowane lub jest zablokowane."}
+                {"reset.password.exception.account.disabled", "Nie możesz resetować hasła. Twoje konto nie zostało jeszcze aktywowane."},
+                {"reset.password.exception.account.expired", "Nie możesz resetować hasła. Twoje konto jest nieaktywne."},
+                {"reset.password.exception.account.locked", "Nie możesz resetować hasła. Twoje konto zostało zablokowane."},
+                {"reset.password.exception.credentials.expired", "Nie możesz resetować hasła. Twoje dane uwierzytelniające straciły ważność."}
         }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
     }
 
@@ -66,8 +68,10 @@ public class Messages {
                 {"registration.confirmation.token.error", "An error occurred while registering. Please contact the administrator."},
                 {"reset.password.error", "An error occurred while resetting password. Please contact the administrator."},
                 {"reset.password", "A link to change the password has been sent to the e-mail address provided."},
-                {"reset.password.account.status.exception",
-                        "You cannot reset your password. Your account has not been activated yet or is blocked."}
+                {"reset.password.exception.account.disabled", "You cannot reset your password. Your account has not been activated yet."},
+                {"reset.password.exception.account.expired", "You cannot reset your password. Your account has expired."},
+                {"reset.password.exception.account.locked", "You cannot reset your password. Your account has been blocked."},
+                {"reset.password.exception.credentials.expired", "You cannot reset your password. Your credentials has expired."}
         }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
     }
 

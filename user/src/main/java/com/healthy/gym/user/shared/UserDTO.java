5850f9fa5001f9847cce340eda7fd36772430019
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-11 16:20:29 +0200

    Update UseDTO - part2

--- user/src/main/java/com/healthy/gym/user/shared/UserDTO.java
+++ user/src/main/java/com/healthy/gym/user/shared/UserDTO.java
@@ -119,6 +119,38 @@ public class UserDTO {
         this.encryptedPassword = encryptedPassword;
     }
 
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;

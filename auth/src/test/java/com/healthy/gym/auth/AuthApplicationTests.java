6c688acbb8f914a23c52f028f30f2b15d1046713
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-17 13:39:43 +0200

    Rename UserApplication.java to AuthApplication.java

--- auth/src/test/java/com/healthy/gym/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/healthy/gym/auth/AuthApplicationTests.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @SpringBootTest
-class UserApplicationTests {
+class AuthApplicationTests {
 
     @Autowired
     private ApplicationContext applicationContext;

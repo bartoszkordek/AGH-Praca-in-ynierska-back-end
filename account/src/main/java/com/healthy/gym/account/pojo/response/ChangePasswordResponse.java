79b488da90007a4f5c5d3defa7a1a52753a9b3c9
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-19 21:13:45 +0200

    Add ChangePasswordResponse

--- account/src/main/java/com/healthy/gym/account/pojo/response/ChangePasswordResponse.java
+++ account/src/main/java/com/healthy/gym/account/pojo/response/ChangePasswordResponse.java
@@ -1,4 +1,11 @@
 package com.healthy.gym.account.pojo.response;
 
-public class ChangePasswordResponse extends AbstractResponse{
+public class ChangePasswordResponse extends AbstractResponse {
+
+    public ChangePasswordResponse() {
+    }
+
+    public ChangePasswordResponse(String message) {
+        super(message);
+    }
 }

03dd6e20ff3fc504324520037172984f9e7d4c57
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-19 20:23:34 +0200

    Add delete account and its tests

--- account/src/main/java/com/healthy/gym/account/controller/AccountController.java
+++ account/src/main/java/com/healthy/gym/account/controller/AccountController.java
@@ -1,22 +1,30 @@
 package com.healthy.gym.account.controller;
 
+import com.healthy.gym.account.component.Translator;
+import com.healthy.gym.account.pojo.response.DeleteAccountResponse;
 import com.healthy.gym.account.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/account")
 public class AccountController {
 
     private final AccountService accountService;
+    private final Translator translator;
 
     @Autowired
-    public AccountController(AccountService accountService) {
+    public AccountController(
+            AccountService accountService,
+            Translator translator
+    ) {
         this.accountService = accountService;
+        this.translator = translator;
     }
 
     @GetMapping("/status")
@@ -42,10 +50,28 @@ public class AccountController {
         return "ok";
     }
 
-    @GetMapping("/deleteAccount")
-    public String deleteAccount() {
-        return "ok";
+    @PreAuthorize("hasRole('ADMIN') or principal==#userId")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<DeleteAccountResponse> deleteAccount(@PathVariable("id") String userId) {
+        try {
+            accountService.deleteAccount(userId);
+
+            String message = translator.toLocale("delete.account.success");
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new DeleteAccountResponse(message));
+
+        } catch (UsernameNotFoundException exception) {
+            String reason=translator.toLocale("exception.account.not.found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, reason, exception);
+
+        } catch (Exception exception) {
+            String reason = translator.toLocale("request.failure");
+            exception.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, reason, exception);
+        }
     }
+
 
     @PostMapping(
             value = "/changeUserSettings",

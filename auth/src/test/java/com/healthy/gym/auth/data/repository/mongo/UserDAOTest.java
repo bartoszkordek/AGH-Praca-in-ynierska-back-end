3941a99cdc26cd6fc04581e42961e6927c32d5d8
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-21 21:11:12 +0200

    Add TestContainers to UserDAOTest

--- auth/src/test/java/com/healthy/gym/auth/data/repository/mongo/UserDAOTest.java
+++ auth/src/test/java/com/healthy/gym/auth/data/repository/mongo/UserDAOTest.java
@@ -5,24 +5,40 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataMongoTest
-@TestPropertySource(properties = {"spring.config.location=classpath:application-test.yaml"})
+@Testcontainers
+@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
 class UserDAOTest {
+    @Container
+    static MongoDBContainer mongoDBContainer =
+            new MongoDBContainer(DockerImageName.parse("mongo:4.4.4-bionic"));
+
     private UserDocument janKowalski, mariaNowak, andrzejNowak;
+
     @Autowired
     private MongoTemplate mongoTemplate;
     @Autowired
     private UserDAO userDAO;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @BeforeEach
     void setUp() {
@@ -68,7 +84,6 @@ class UserDAOTest {
     @Test
     void shouldReturnProperUserDocumentWhenFindByEmailIsCalled() {
         UserDocument found = userDAO.findByEmail("jan.kowalski@test.com");
-        System.out.println(found);
         assertThat(found)
                 .isEqualTo(janKowalski)
                 .hasSameHashCodeAs(janKowalski);
@@ -83,7 +98,6 @@ class UserDAOTest {
     @Test
     void shouldReturnAllUsersInDatabase() {
         List<UserDocument> found = userDAO.findAll();
-        System.out.println(found);
         assertThat(found)
                 .isNotNull()
                 .hasSize(3)

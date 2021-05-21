244487cbbaa7bcb5a6ac48a3618904b487254eae
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-21 21:10:44 +0200

    Update MongoConfiguration

--- auth/src/main/java/com/healthy/gym/auth/configuration/MongoConfiguration.java
+++ auth/src/main/java/com/healthy/gym/auth/configuration/MongoConfiguration.java
@@ -1,15 +1,13 @@
 package com.healthy.gym.auth.configuration;
 
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-import java.util.Collections;
 
 @EnableMongoRepositories(basePackages = "com.healthy.gym.auth.data.repository")
 @Configuration
@@ -27,32 +25,9 @@ public class MongoConfiguration extends AbstractMongoClientConfiguration {
         return environment.getRequiredProperty("spring.data.mongodb.database");
     }
 
-    @Override
-    protected void configureClientSettings(MongoClientSettings.Builder builder) {
-        MongoCredential mongoCredential = MongoCredential
-                .createCredential(getUsername(), getDatabaseName(), getPassword());
-
-        ServerAddress serverAddress = new ServerAddress(getHost(), getPort());
-
-        builder.credential(mongoCredential)
-                .applyToClusterSettings(settings -> settings.hosts(Collections.singletonList(serverAddress)));
-    }
-
-    private String getHost() {
-        return environment.getRequiredProperty("spring.data.mongodb.host");
-    }
-
-    private String getUsername() {
-        return environment.getRequiredProperty("spring.data.mongodb.username");
-    }
-
-    private char[] getPassword() {
-        String password = environment.getRequiredProperty("spring.data.mongodb.password");
-        return password.toCharArray();
-    }
-
-    private int getPort() {
-        String port = environment.getRequiredProperty("spring.data.mongodb.port");
-        return Integer.parseInt(port);
+    @Bean
+    public MongoClient mongoClient() {
+        String uri = environment.getRequiredProperty("spring.data.mongodb.uri");
+        return MongoClients.create(uri);
     }
 }

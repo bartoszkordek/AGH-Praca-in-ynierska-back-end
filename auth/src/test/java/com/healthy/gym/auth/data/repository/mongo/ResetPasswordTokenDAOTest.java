19ab6882625ca71567c0971e2495464201e3586e
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-21 21:29:00 +0200

    Add TestContainers to RegistrationTokenDAOTest and ResetPasswordTokenDAOTest

--- auth/src/test/java/com/healthy/gym/auth/data/repository/mongo/ResetPasswordTokenDAOTest.java
+++ auth/src/test/java/com/healthy/gym/auth/data/repository/mongo/ResetPasswordTokenDAOTest.java
@@ -7,29 +7,43 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataMongoTest
-@TestPropertySource(properties = {"spring.config.location=classpath:application-test.yaml"})
+@Testcontainers
+@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
 class ResetPasswordTokenDAOTest {
+
+    @Container
+    static MongoDBContainer mongoDBContainer =
+            new MongoDBContainer(DockerImageName.parse("mongo:4.4.4-bionic"));
     private UserDocument janKowalski, andrzejNowak;
     private ResetPasswordTokenDocument resetPasswordToken;
     private String testToken;
-
     @Autowired
     private MongoTemplate mongoTemplate;
     @Autowired
     private UserDAO userDAO;
     @Autowired
     private ResetPasswordTokenDAO resetPasswordTokenDAO;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @BeforeEach
     void setUp() {

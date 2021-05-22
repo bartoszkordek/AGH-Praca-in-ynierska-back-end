1d3f3a6143274c07dbcaa69a6b2ee4c22f89876d
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-22 10:55:52 +0200

    Add implementation of deleteAccount() in AccountServiceImpl. Add its tests.

--- account/src/main/java/com/healthy/gym/account/service/AccountServiceImpl.java
+++ account/src/main/java/com/healthy/gym/account/service/AccountServiceImpl.java
@@ -1,10 +1,26 @@
 package com.healthy.gym.account.service;
 
+import com.healthy.gym.account.data.document.UserDocument;
+import com.healthy.gym.account.data.repository.UserDAO;
 import com.healthy.gym.account.shared.UserDTO;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountServiceImpl implements AccountService {
+    private final UserDAO userDAO;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public AccountServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+        modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    }
+
     @Override
     public UserDTO changePassword(String password) {
         return null;
@@ -12,6 +28,12 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public UserDTO deleteAccount(String userId) {
-        return null;
+        UserDocument foundUser = userDAO.findByUserId(userId);
+
+        if (foundUser == null) throw new UsernameNotFoundException("User not found.");
+
+        userDAO.delete(foundUser);
+
+        return modelMapper.map(foundUser, UserDTO.class);
     }
 }

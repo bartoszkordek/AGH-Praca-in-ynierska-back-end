9fe1472a31192eba0e5525a5589ab99492e31c73
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-23 01:43:44 +0200

    Add changePassword in AccountService and its tests.

--- account/src/test/java/com/healthy/gym/account/service/accountServiceTest/WhenChangePasswordTest.java
+++ account/src/test/java/com/healthy/gym/account/service/accountServiceTest/WhenChangePasswordTest.java
@@ -2,24 +2,31 @@ package com.healthy.gym.account.service.accountServiceTest;
 
 import com.healthy.gym.account.data.document.UserDocument;
 import com.healthy.gym.account.data.repository.UserDAO;
+import com.healthy.gym.account.exception.IdenticalOldAndNewPasswordException;
+import com.healthy.gym.account.exception.OldPasswordDoesNotMatchException;
 import com.healthy.gym.account.service.AccountService;
 import com.healthy.gym.account.shared.UserDTO;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
-public class WhenChangePasswordTest {
+class WhenChangePasswordTest {
 
     private UserDTO andrzejNowakDTO;
     private UserDocument andrzejNowak;
+    private UserDocument andrzejNowakUpdated;
     private String userId;
 
     @Autowired
@@ -33,8 +40,17 @@ public class WhenChangePasswordTest {
 
     @BeforeEach
     void setUp() {
-        String encryptedPassword = bCryptPasswordEncoder.encode("password4576");
         userId = UUID.randomUUID().toString();
+        andrzejNowak = new UserDocument(
+                "Andrzej",
+                "Nowak",
+                "andrzej.nowak@test.com",
+                "676 777 888",
+                bCryptPasswordEncoder.encode("password4576"),
+                userId
+        );
+
+        String encryptedPasswordUpdated = bCryptPasswordEncoder.encode("password45768");
 
         andrzejNowakDTO = new UserDTO(
                 userId,
@@ -43,19 +59,49 @@ public class WhenChangePasswordTest {
                 "andrzej.nowak@test.com",
                 "676 777 888",
                 null,
-                encryptedPassword
+                encryptedPasswordUpdated
         );
 
-        andrzejNowak = new UserDocument(
+        andrzejNowakUpdated = new UserDocument(
                 "Andrzej",
                 "Nowak",
                 "andrzej.nowak@test.com",
                 "676 777 888",
-                encryptedPassword,
+                encryptedPasswordUpdated,
                 userId
         );
-
-        doNothing().when(userDAO).delete(any(UserDocument.class));
     }
 
+    @Test
+    void shouldThrowExceptionWhenUserIsNotFound() {
+        when(userDAO.findByUserId(userId)).thenReturn(null);
+        assertThatThrownBy(
+                () -> accountService.changePassword(userId, "test1234", "test12345")
+        ).isInstanceOf(UsernameNotFoundException.class);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenOldPasswordDoesNotMatch() {
+        when(userDAO.findByUserId(userId)).thenReturn(andrzejNowak);
+        assertThatThrownBy(
+                () -> accountService.changePassword(userId, "test1234", "test12345")
+        ).isInstanceOf(OldPasswordDoesNotMatchException.class);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenNewPasswordIsEqualToOldPassword() {
+        when(userDAO.findByUserId(userId)).thenReturn(andrzejNowak);
+        assertThatThrownBy(
+                () -> accountService.changePassword(userId, "password4576", "password4576")
+        ).isInstanceOf(IdenticalOldAndNewPasswordException.class);
+    }
+
+    @Test
+    void shouldUpdatePassword()
+            throws OldPasswordDoesNotMatchException, IdenticalOldAndNewPasswordException {
+        when(userDAO.findByUserId(userId)).thenReturn(andrzejNowak);
+        when(userDAO.save(any())).thenReturn(andrzejNowakUpdated);
+        assertThat(accountService.changePassword(userId, "password4576", "password45768"))
+                .isEqualTo(andrzejNowakDTO);
+    }
 }

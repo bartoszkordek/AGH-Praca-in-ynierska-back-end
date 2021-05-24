5248bc82f19c874a5c7828cf46741888edc691c3
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-24 20:33:32 +0200

    Add ResponseBindException and its @ExceptionHandler

--- account/src/main/java/com/healthy/gym/account/exception/RestExceptionHandler.java
+++ account/src/main/java/com/healthy/gym/account/exception/RestExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -34,5 +37,27 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         body.put("message", reason);
 
         return new ResponseEntity<>(body, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler({ResponseBindException.class})
+    public ResponseEntity<Object> handleBindException(ResponseBindException exception) {
+        String reason = exception.getReason();
+        HttpStatus httpStatus = exception.getHttpStatus();
+        BindException bindException = exception.getException();
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", httpStatus.value());
+        body.put("error", httpStatus.getReasonPhrase());
+        body.put("message", reason);
+        body.put("errors", getBindExceptionErrorMessages(bindException));
+
+        return new ResponseEntity<>(body, httpStatus);
+    }
+
+    private Map<String, String> getBindExceptionErrorMessages(BindException exception) {
+        return exception.getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
     }
 }

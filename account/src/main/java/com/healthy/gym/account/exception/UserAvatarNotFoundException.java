168f3d57e2030c1fc6d1c327239a2aee0cf2d296
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-25 11:47:56 +0200

    Add UserAvatarNotFoundException

---
+++ account/src/main/java/com/healthy/gym/account/exception/UserAvatarNotFoundException.java
@@ -0,0 +1,10 @@
+package com.healthy.gym.account.exception;
+
+public class UserAvatarNotFoundException extends Exception {
+    public UserAvatarNotFoundException() {
+    }
+
+    public UserAvatarNotFoundException(String message) {
+        super(message);
+    }
+}

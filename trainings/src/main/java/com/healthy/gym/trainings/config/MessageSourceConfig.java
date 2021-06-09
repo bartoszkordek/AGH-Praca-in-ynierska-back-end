7d3dfab2b2f27ec6004e412dc449c1a0a027a47a
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-09 21:01:57 +0200

    Add MessageSourceConfig and initial messages.

---
+++ trainings/src/main/java/com/healthy/gym/trainings/config/MessageSourceConfig.java
@@ -0,0 +1,49 @@
+package com.healthy.gym.trainings.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+@Configuration
+@ComponentScan({"com.healthy.gym.trainings"})
+public class MessageSourceConfig extends AcceptHeaderLocaleResolver implements WebMvcConfigurer {
+
+    private final Locale poland = new Locale("pl", "PL");
+    private final List<Locale> locales = Arrays.asList(poland, Locale.ENGLISH);
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(10);
+        return messageSource;
+    }
+
+    @Bean
+    @Override
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String headerLang = request.getHeader("Accept-Language");
+        if (headerLang == null || headerLang.isEmpty()) return Locale.getDefault();
+        return Locale.lookup(Locale.LanguageRange.parse(headerLang), locales);
+    }
+}

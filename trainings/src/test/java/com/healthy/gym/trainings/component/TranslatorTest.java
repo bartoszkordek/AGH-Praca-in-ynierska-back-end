d82e4917c6f3c62de20e5abc38ca991996b3648d
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-09 21:02:40 +0200

    Add Translator component and its tests.

---
+++ trainings/src/test/java/com/healthy/gym/trainings/component/TranslatorTest.java
@@ -0,0 +1,33 @@
+package com.healthy.gym.trainings.component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TranslatorTest {
+    private final Locale poland = new Locale("pl", "PL");
+    private final String property = "field.required";
+
+    @Autowired
+    private Translator translator;
+
+    @Test
+    void shouldReturnEnglishMessage() {
+        LocaleContextHolder.setLocale(Locale.ENGLISH);
+        String messageAccordingToLocale = translator.toLocale(property);
+        assertThat(messageAccordingToLocale).isEqualTo("Field is required.");
+    }
+
+    @Test
+    void shouldReturnPolishMessage() {
+        LocaleContextHolder.setLocale(poland);
+        String messageAccordingToLocale = translator.toLocale(property);
+        assertThat(messageAccordingToLocale).isEqualTo("Pole jest wymagane.");
+    }
+}

2950fd7dbdf281bc9bdc7d98956d53a6cdd381c6
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-09 12:22:41 +0200

    Refactor TrainingTypeService: add its interface and concrete class. Add initial test class.

---
+++ trainings/src/test/java/com/healthy/gym/trainings/service/TrainingTypeServiceTest.java
@@ -0,0 +1,21 @@
+package com.healthy.gym.trainings.service;
+
+import com.healthy.gym.trainings.db.TrainingTypeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+class TrainingTypeServiceTest {
+
+    @Autowired
+    private TrainingTypeService trainingTypeService;
+
+    @MockBean
+    private TrainingTypeRepository trainingTypeRepository;
+
+    @Test
+    void name() {
+    }
+}

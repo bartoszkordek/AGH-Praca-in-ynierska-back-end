27add79205774a1d7433016f73a251fbaa03bd58
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-09 13:16:13 +0200

    Add enum GymRole and its tests.

---
+++ trainings/src/main/java/com/healthy/gym/trainings/enums/GymRole.java
@@ -0,0 +1,31 @@
+package com.healthy.gym.trainings.enums;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum GymRole implements GrantedAuthority {
+    ADMIN("ROLE_ADMIN"),
+    EMPLOYEE("ROLE_EMPLOYEE"),
+    TRAINER("ROLE_TRAINER"),
+    USER("ROLE_USER");
+
+    private final String role;
+
+    GymRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.role;
+    }
+
+    @Override
+    public String toString() {
+        return this.role;
+    }
+}
+

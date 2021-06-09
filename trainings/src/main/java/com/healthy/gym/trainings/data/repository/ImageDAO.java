12f013826b83ee63693a128a773d4281d36f2ca6
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-09 17:07:57 +0200

    Add ImageDAO and its integration tests.

---
+++ trainings/src/main/java/com/healthy/gym/trainings/data/repository/ImageDAO.java
@@ -0,0 +1,8 @@
+package com.healthy.gym.trainings.data.repository;
+
+import com.healthy.gym.trainings.data.document.ImageDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ImageDAO extends MongoRepository<ImageDocument, String> {
+    ImageDocument findByImageId(String imageId);
+}

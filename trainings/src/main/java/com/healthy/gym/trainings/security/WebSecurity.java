878d199252923dd1682b6b6b37f9f97ec649590a
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-10 08:49:55 +0200

    Add AuthorizationFilter and WebSecurity to trainings microservice.

--- trainings/src/main/java/com/healthy/gym/trainings/security/WebSecurity.java
+++ trainings/src/main/java/com/healthy/gym/trainings/security/WebSecurity.java
@@ -1,9 +1,40 @@
 package com.healthy.gym.trainings.security;
 
-import org.springframework.context.annotation.Configuration;
+import com.healthy.gym.trainings.component.TokenManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
-@Configuration
-public class WebSecurity {
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-    //TODO implement WebSecurity
+    private final TokenManager tokenManager;
+
+    @Autowired
+    public WebSecurity(TokenManager tokenManager) {
+        this.tokenManager = tokenManager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/status").authenticated()
+                .and()
+                .authorizeRequests().antMatchers("/*").permitAll()
+                .and()
+                .addFilter(getAuthorizationFilter());
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    private AuthorizationFilter getAuthorizationFilter() throws Exception {
+        return new AuthorizationFilter(authenticationManager(), tokenManager);
+    }
 }

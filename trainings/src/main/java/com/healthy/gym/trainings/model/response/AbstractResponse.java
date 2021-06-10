a64a2f9d87516852cf3dd74b19d643e2c2a1b89b
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-10 20:49:43 +0200

    Add AbstractResponse and TrainingTypeResponse.

---
+++ trainings/src/main/java/com/healthy/gym/trainings/model/response/AbstractResponse.java
@@ -0,0 +1,56 @@
+package com.healthy.gym.trainings.model.response;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Objects;
+
+public abstract class AbstractResponse implements Serializable {
+    private String message;
+    private Map<String, String> errors;
+
+    protected AbstractResponse() {
+    }
+
+    protected AbstractResponse(String message, Map<String, String> errors) {
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Map<String, String> errors) {
+        this.errors = errors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractResponse that = (AbstractResponse) o;
+        return Objects.equals(message, that.message)
+                && Objects.equals(errors, that.errors);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message, errors);
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractResponse{" +
+                "message='" + message + '\'' +
+                ", errors=" + errors +
+                '}';
+    }
+}
---
+++ trainings/src/main/java/com/healthy/gym/trainings/model/response/TrainingTypeResponse.java
@@ -0,0 +1,79 @@
+package com.healthy.gym.trainings.model.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class TrainingTypeResponse extends AbstractResponse {
+    @JsonProperty("image")
+    public String imageBase64Encoded;
+
+    private String name;
+    private String description;
+
+    public TrainingTypeResponse() {
+    }
+
+    public TrainingTypeResponse(
+            String message,
+            Map<String, String> errors,
+            String imageBase64Encoded,
+            String name,
+            String description
+    ) {
+        super(message, errors);
+        this.imageBase64Encoded = imageBase64Encoded;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getImageBase64Encoded() {
+        return imageBase64Encoded;
+    }
+
+    public void setImageBase64Encoded(String imageBase64Encoded) {
+        this.imageBase64Encoded = imageBase64Encoded;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        TrainingTypeResponse that = (TrainingTypeResponse) o;
+        return Objects.equals(imageBase64Encoded, that.imageBase64Encoded)
+                && Objects.equals(name, that.name)
+                && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), imageBase64Encoded, name, description);
+    }
+
+    @Override
+    public String toString() {
+        return "TrainingTypeResponse{" +
+                "imageBase64Encoded='" + imageBase64Encoded + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                "} " + super.toString();
+    }
+}

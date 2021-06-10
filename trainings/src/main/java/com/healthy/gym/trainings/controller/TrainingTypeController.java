52858c641d453cf6466e59fe2a4f1741c9a286d3
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-10 20:51:14 +0200

    Add layer tests and implementation for createTrainingType().

--- trainings/src/main/java/com/healthy/gym/trainings/controller/TrainingTypeController.java
+++ trainings/src/main/java/com/healthy/gym/trainings/controller/TrainingTypeController.java
@@ -1,19 +1,31 @@
 package com.healthy.gym.trainings.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.healthy.gym.trainings.component.ImageValidator;
+import com.healthy.gym.trainings.component.MultipartFileValidator;
+import com.healthy.gym.trainings.component.Translator;
 import com.healthy.gym.trainings.data.document.TrainingTypeDocument;
-import com.healthy.gym.trainings.exception.DuplicatedTrainingTypes;
+import com.healthy.gym.trainings.exception.DuplicatedTrainingTypeException;
+import com.healthy.gym.trainings.exception.MultipartBodyException;
 import com.healthy.gym.trainings.exception.NotExistingTrainingType;
 import com.healthy.gym.trainings.exception.RestException;
 import com.healthy.gym.trainings.model.other.TrainingTypeModel;
+import com.healthy.gym.trainings.model.request.TrainingTypeRequest;
 import com.healthy.gym.trainings.model.response.TrainingTypePublicResponse;
+import com.healthy.gym.trainings.model.response.TrainingTypeResponse;
 import com.healthy.gym.trainings.service.TrainingTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.activation.UnsupportedDataTypeException;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 @RestController
@@ -24,25 +36,75 @@ public class TrainingTypeController {
     private static final String GENERIC_ERROR_MESSAGE_WHILE_ISSUES_WITH_PHOTO_PROCESSING =
             "Errors while proto processing.";
     private final TrainingTypeService trainingTypeService;
+    private final ObjectMapper objectMapper;
+    private final Translator translator;
+    private final MultipartFileValidator multipartFileValidator;
+    private final ImageValidator imageValidator;
 
     @Autowired
-    public TrainingTypeController(TrainingTypeService trainingTypeService) {
+    public TrainingTypeController(
+            TrainingTypeService trainingTypeService,
+            Translator translator,
+            MultipartFileValidator multipartFileValidator,
+            ImageValidator imageValidator
+    ) {
         this.trainingTypeService = trainingTypeService;
+        this.translator = translator;
+        this.multipartFileValidator = multipartFileValidator;
+        this.imageValidator = imageValidator;
+        this.objectMapper = new ObjectMapper();
     }
 
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public TrainingTypeDocument createTrainingType(
-            @RequestParam("trainingName") String trainingName,
-            @RequestParam("description") String description,
-            @RequestParam("avatar") MultipartFile multipartFile
-    ) throws RestException {
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER')")
+    @PostMapping(
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<TrainingTypeResponse> createTrainingType(
+            @RequestPart(value = "body") TrainingTypeRequest trainingTypeRequest,
+            @RequestPart(value = "image", required = false) MultipartFile multipartFile
+    ) {
+        TrainingTypeResponse response = new TrainingTypeResponse();
         try {
-            TrainingTypeModel trainingTypeModel = new TrainingTypeModel(trainingName, description);
-            return trainingTypeService.createTrainingType(trainingTypeModel, multipartFile.getBytes());
-        } catch (DuplicatedTrainingTypes e) {
-            throw new RestException(e.getMessage(), HttpStatus.CONFLICT, e);
-        } catch (IOException exception) {
-            throw new RestException(GENERIC_ERROR_MESSAGE_WHILE_ISSUES_WITH_PHOTO_PROCESSING, HttpStatus.BAD_REQUEST, exception);
+            multipartFileValidator.validateBody(trainingTypeRequest);
+            imageValidator.isFileSupported(multipartFile);
+
+            trainingTypeService.createTrainingType(trainingTypeRequest, multipartFile);
+
+            String message = translator.toLocale("training.type.created");
+            response.setMessage(message);
+            response.setName(trainingTypeRequest.getName());
+            response.setDescription(trainingTypeRequest.getDescription());
+            String imageBas64 = Base64.getEncoder().encodeToString(multipartFile.getBytes());
+            response.setImageBase64Encoded(imageBas64);
+
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(response);
+
+        } catch (MultipartBodyException exception) {
+            String message = translator.toLocale("exception.multipart.body");
+            response.setMessage(message);
+            response.setErrors(exception.getErrorMap());
+
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(response);
+
+        } catch (UnsupportedDataTypeException exception) {
+            String reason = translator.toLocale("exception.unsupported.data.type");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, reason, exception);
+
+        } catch (DuplicatedTrainingTypeException exception) {
+            String reason = translator.toLocale("exception.duplicated.training.type");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, reason, exception);
+
+        } catch (Exception exception) {
+            String reason = translator.toLocale("exception.internal.error");
+            exception.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, reason, exception);
         }
     }
 
@@ -82,7 +144,7 @@ public class TrainingTypeController {
         try {
             TrainingTypeModel trainingTypeModel = new TrainingTypeModel(trainingName, description);
             return trainingTypeService.updateTrainingTypeById(trainingTypeId, trainingTypeModel, multipartFile.getBytes());
-        } catch (NotExistingTrainingType | IOException | DuplicatedTrainingTypes e) {
+        } catch (NotExistingTrainingType | IOException | DuplicatedTrainingTypeException e) {
             throw new RestException(e.getMessage(), HttpStatus.BAD_REQUEST, e);
         }
     }
--- trainings/src/test/java/com/healthy/gym/trainings/controller/trainingTypeController/unitTest/WhenCreateTrainingTypeTest.java
+++ trainings/src/test/java/com/healthy/gym/trainings/controller/trainingTypeController/unitTest/WhenCreateTrainingTypeTest.java
@@ -1,25 +1,333 @@
 package com.healthy.gym.trainings.controller.trainingTypeController.unitTest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.healthy.gym.trainings.configuration.TestCountry;
+import com.healthy.gym.trainings.configuration.TestRoleTokenFactory;
 import com.healthy.gym.trainings.controller.TrainingTypeController;
+import com.healthy.gym.trainings.exception.DuplicatedTrainingTypeException;
+import com.healthy.gym.trainings.model.request.TrainingTypeRequest;
 import com.healthy.gym.trainings.service.TrainingTypeService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.activation.UnsupportedDataTypeException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.healthy.gym.trainings.configuration.LocaleConverter.convertEnumToLocale;
+import static com.healthy.gym.trainings.configuration.Messages.getMessagesAccordingToLocale;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.ResultMatcher.matchAll;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(TrainingTypeController.class)
-public class WhenCreateTrainingTypeTest {
+class WhenCreateTrainingTypeTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private TestRoleTokenFactory tokenFactory;
 
     @MockBean
     private TrainingTypeService trainingTypeService;
 
-    @Test
-    void name() {
-        assertThat(true).isTrue();
+    private String managerToken;
+    private String adminToken;
+    private String userToken;
+    private MockMultipartFile invalidFile;
+    private MockMultipartFile validFile;
+    private MockMultipartFile validBody;
+    private MockMultipartFile invalidBody;
+    private TrainingTypeRequest trainingTypeRequestValid;
+    private TrainingTypeRequest trainingTypeRequestInvalid;
+
+
+    @BeforeEach
+    void setUp() throws JsonProcessingException {
+        String userId = UUID.randomUUID().toString();
+        userToken = tokenFactory.getUserToken(userId);
+
+        String managerId = UUID.randomUUID().toString();
+        managerToken = tokenFactory.getMangerToken(managerId);
+
+        String adminId = UUID.randomUUID().toString();
+        adminToken = tokenFactory.getAdminToken(adminId);
+
+        invalidFile = new MockMultipartFile(
+                "image",
+                "hello.txt",
+                MediaType.TEXT_PLAIN_VALUE,
+                "Hello, World!".getBytes(StandardCharsets.UTF_8)
+        );
+
+        validFile = new MockMultipartFile(
+                "image",
+                "hello.png",
+                MediaType.IMAGE_PNG_VALUE,
+                "data".getBytes(StandardCharsets.UTF_8)
+        );
+
+        trainingTypeRequestValid = new TrainingTypeRequest();
+        trainingTypeRequestValid.setName("Test name");
+        trainingTypeRequestValid.setDescription("Test description");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonBody = objectMapper.writeValueAsString(trainingTypeRequestValid);
+
+        validBody = new MockMultipartFile(
+                "body",
+                "body.json",
+                MediaType.APPLICATION_JSON_VALUE,
+                jsonBody.getBytes(StandardCharsets.UTF_8)
+        );
+
+        trainingTypeRequestInvalid = new TrainingTypeRequest();
+        trainingTypeRequestInvalid.setName("a");
+        trainingTypeRequestInvalid.setDescription("T");
+        String invalidJsonBody = objectMapper.writeValueAsString(trainingTypeRequestInvalid);
+
+        invalidBody = new MockMultipartFile(
+                "body",
+                "body.json",
+                MediaType.APPLICATION_JSON_VALUE,
+                invalidJsonBody.getBytes(StandardCharsets.UTF_8)
+        );
+
+    }
+
+    @Nested
+    class ShouldAcceptRequestWhenUserHasAdminOrManagerRoleAnd {
+        @ParameterizedTest
+        @EnumSource(TestCountry.class)
+        void shouldCreateNewTrainingType(TestCountry country) throws Exception {
+            Map<String, String> messages = getMessagesAccordingToLocale(country);
+            Locale testedLocale = convertEnumToLocale(country);
+
+            URI uri = new URI("/trainingType");
+
+            RequestBuilder request = MockMvcRequestBuilders
+                    .multipart(uri)
+                    .file(validFile)
+                    .file(validBody)
+                    .header("Accept-Language", testedLocale.toString())
+                    .header("Authorization", adminToken)
+                    .contentType(MediaType.MULTIPART_FORM_DATA_VALUE);
+
+            String expectedMessage = messages.get("training.type.created");
+
+            mockMvc.perform(request)
+                    .andDo(print())
+                    .andExpect(matchAll(
+                            status().isCreated(),
+                            content().contentType(MediaType.APPLICATION_JSON),
+                            jsonPath("$.message").value(is(expectedMessage)),
+                            jsonPath("$.errors").value(is(nullValue())),
+                            jsonPath("$.image").isNotEmpty(),
+                            jsonPath("$.name").value(is("Test name")),
+                            jsonPath("$.description").value(is("Test description"))
+                    ));
+        }
+
+        @ParameterizedTest
+        @EnumSource(TestCountry.class)
+        void shouldThrowUnsupportedDataTypeExceptionWhenInvalidFileProvided(TestCountry country) throws Exception {
+            Map<String, String> messages = getMessagesAccordingToLocale(country);
+            Locale testedLocale = convertEnumToLocale(country);
+
+            URI uri = new URI("/trainingType");
+
+            String expectedMessage = messages.get("exception.unsupported.data.type");
+
+            RequestBuilder request = MockMvcRequestBuilders
+                    .multipart(uri)
+                    .file(invalidFile)
+                    .file(validBody)
+                    .header("Accept-Language", testedLocale.toString())
+                    .header("Authorization", managerToken)
+                    .contentType(MediaType.MULTIPART_FORM_DATA_VALUE);
+
+            mockMvc.perform(request)
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(status().reason(is(expectedMessage)))
+                    .andExpect(result ->
+                            assertThat(result.getResolvedException().getCause())
+                                    .isInstanceOf(UnsupportedDataTypeException.class)
+                    );
+        }
+
+        @ParameterizedTest
+        @EnumSource(TestCountry.class)
+        void shouldMultipartBodyExceptionWhenInvalidBody(TestCountry country) throws Exception {
+            Map<String, String> messages = getMessagesAccordingToLocale(country);
+            Locale testedLocale = convertEnumToLocale(country);
+
+            URI uri = new URI("/trainingType");
+
+            RequestBuilder request = MockMvcRequestBuilders
+                    .multipart(uri)
+                    .file(validFile)
+                    .file(invalidBody)
+                    .header("Accept-Language", testedLocale.toString())
+                    .header("Authorization", managerToken)
+                    .contentType(MediaType.MULTIPART_FORM_DATA_VALUE);
+
+            String expectedMessage = messages.get("exception.multipart.body");
+
+            mockMvc.perform(request)
+                    .andDo(print())
+                    .andExpect(matchAll(
+                            status().isBadRequest(),
+                            content().contentType(MediaType.APPLICATION_JSON),
+                            jsonPath("$.message").value(is(expectedMessage)),
+                            jsonPath("$.errors").value(is(notNullValue())),
+                            jsonPath("$.errors.name")
+                                    .value(is(messages.get("field.required"))),
+                            jsonPath("$.errors.description")
+                                    .value(is(messages.get("field.required"))),
+                            jsonPath("$.image").value(is(nullValue())),
+                            jsonPath("$.name").value(is(nullValue())),
+                            jsonPath("$.description").value(is(nullValue()))
+                    ));
+        }
+
+        @ParameterizedTest
+        @EnumSource(TestCountry.class)
+        void shouldThrowDuplicatedTrainingTypeExceptionWhenTrainingTypeNameAlreadyExists(TestCountry country)
+                throws Exception {
+            Map<String, String> messages = getMessagesAccordingToLocale(country);
+            Locale testedLocale = convertEnumToLocale(country);
+
+            URI uri = new URI("/trainingType");
+
+            RequestBuilder request = MockMvcRequestBuilders
+                    .multipart(uri)
+                    .file(validFile)
+                    .file(validBody)
+                    .header("Accept-Language", testedLocale.toString())
+                    .header("Authorization", managerToken)
+                    .contentType(MediaType.MULTIPART_FORM_DATA_VALUE);
+
+            doThrow(DuplicatedTrainingTypeException.class)
+                    .when(trainingTypeService)
+                    .createTrainingType(
+                            ArgumentMatchers.any(TrainingTypeRequest.class),
+                            ArgumentMatchers.any(MockMultipartFile.class)
+                    );
+
+            String expectedMessage = messages.get("exception.duplicated.training.type");
+
+            mockMvc.perform(request)
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(status().reason(is(expectedMessage)))
+                    .andExpect(result ->
+                            assertThat(result.getResolvedException().getCause())
+                                    .isInstanceOf(DuplicatedTrainingTypeException.class)
+                    );
+        }
+
+        @ParameterizedTest
+        @EnumSource(TestCountry.class)
+        void shouldThrowIllegalStateExceptionWhenInternalErrorOccurs(TestCountry country)
+                throws Exception {
+            Map<String, String> messages = getMessagesAccordingToLocale(country);
+            Locale testedLocale = convertEnumToLocale(country);
+
+            URI uri = new URI("/trainingType");
+
+            RequestBuilder request = MockMvcRequestBuilders
+                    .multipart(uri)
+                    .file(validFile)
+                    .file(validBody)
+                    .header("Accept-Language", testedLocale.toString())
+                    .header("Authorization", managerToken)
+                    .contentType(MediaType.MULTIPART_FORM_DATA_VALUE);
+
+            doThrow(IllegalStateException.class)
+                    .when(trainingTypeService)
+                    .createTrainingType(
+                            ArgumentMatchers.any(TrainingTypeRequest.class),
+                            ArgumentMatchers.any(MockMultipartFile.class)
+                    );
+
+            String expectedMessage = messages.get("exception.internal.error");
+
+            mockMvc.perform(request)
+                    .andDo(print())
+                    .andExpect(status().isInternalServerError())
+                    .andExpect(status().reason(is(expectedMessage)))
+                    .andExpect(result ->
+                            assertThat(result.getResolvedException().getCause())
+                                    .isInstanceOf(IllegalStateException.class)
+                    );
+        }
+    }
+
+    @Nested
+    class ShouldRejectRequest {
+        @ParameterizedTest
+        @EnumSource(TestCountry.class)
+        void whenUserDoesNotHaveAdminOrManagerRole(TestCountry country) throws Exception {
+            Map<String, String> messages = getMessagesAccordingToLocale(country);
+            Locale testedLocale = convertEnumToLocale(country);
+
+            URI uri = new URI("/trainingType");
+
+            RequestBuilder request = MockMvcRequestBuilders
+                    .multipart(uri)
+                    .file(validFile)
+                    .file(validBody)
+                    .header("Accept-Language", testedLocale.toString())
+                    .header("Authorization", userToken)
+                    .contentType(MediaType.MULTIPART_FORM_DATA_VALUE);
+
+            String expectedMessage = messages.get("exception.access.denied");
+
+            mockMvc.perform(request)
+                    .andDo(print())
+                    .andExpect(status().isForbidden())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(jsonPath("$.message").value(is(expectedMessage)))
+                    .andExpect(jsonPath("$.error").value(is("Forbidden")))
+                    .andExpect(jsonPath("$.status").value(403))
+                    .andExpect(jsonPath("$.timestamp").exists());
+        }
+
+        @ParameterizedTest
+        @EnumSource(TestCountry.class)
+        void whenUserIsNotLogIn(TestCountry country) throws Exception {
+            Locale testedLocale = convertEnumToLocale(country);
+
+            URI uri = new URI("/trainingType");
+
+            RequestBuilder request = MockMvcRequestBuilders
+                    .multipart(uri)
+                    .file(validFile)
+                    .file(validBody)
+                    .header("Accept-Language", testedLocale.toString())
+                    .contentType(MediaType.MULTIPART_FORM_DATA_VALUE);
+
+            mockMvc.perform(request)
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
     }
 }

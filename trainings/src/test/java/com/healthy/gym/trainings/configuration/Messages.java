abde8a9ac1b02424b8519eb41ee40a2b81884cf5
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-10 10:05:12 +0200

    Add message: exception.access.denied

--- trainings/src/test/java/com/healthy/gym/trainings/configuration/Messages.java
+++ trainings/src/test/java/com/healthy/gym/trainings/configuration/Messages.java
@@ -7,12 +7,14 @@ import java.util.stream.Stream;
 public class Messages {
     private static Map<String, String> getMessagesPL() {
         return Stream.of(new String[][]{
+                {"exception.access.denied", "Nie masz uprawnień do wykonania tej operacji."},
                 {"field.required", "Pole jest wymagane."}
         }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
     }
 
     private static Map<String, String> getMessagesEN() {
         return Stream.of(new String[][]{
+                {"exception.access.denied", "You are not allowed to perform this operation."},
                 {"field.required", "Field is required."}
         }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
     }

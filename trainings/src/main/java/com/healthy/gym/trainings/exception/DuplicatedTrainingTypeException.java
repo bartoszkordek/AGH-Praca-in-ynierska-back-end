b8f4797c4b4110b068e1475940cab5a859f9bc05
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-11 21:08:58 +0200

    Add new constructor to DuplicatedTrainingTypeException.

--- trainings/src/main/java/com/healthy/gym/trainings/exception/DuplicatedTrainingTypeException.java
+++ trainings/src/main/java/com/healthy/gym/trainings/exception/DuplicatedTrainingTypeException.java
@@ -1,5 +1,11 @@
 package com.healthy.gym.trainings.exception;
 
-public class DuplicatedTrainingTypeException extends Exception{
-    public DuplicatedTrainingTypeException(String message){super(message);}
+public class DuplicatedTrainingTypeException extends Exception {
+
+    public DuplicatedTrainingTypeException() {
+    }
+
+    public DuplicatedTrainingTypeException(String message) {
+        super(message);
+    }
 }

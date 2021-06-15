61639ae2e435586ded9ee8481ae3c786eaa09f35
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-15 20:08:14 +0200

    Add ImageDTO and its tests.

---
+++ trainings/src/main/java/com/healthy/gym/trainings/shared/ImageDTO.java
@@ -0,0 +1,53 @@
+package com.healthy.gym.trainings.shared;
+
+import java.util.Objects;
+
+public class ImageDTO {
+    private String data;
+    private String format;
+
+    public ImageDTO() {
+    }
+
+    public ImageDTO(String data, String format) {
+        this.data = data;
+        this.format = format;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ImageDTO imageDTO = (ImageDTO) o;
+        return Objects.equals(data, imageDTO.data) && Objects.equals(format, imageDTO.format);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data, format);
+    }
+
+    @Override
+    public String toString() {
+        return "ImageDTO{" +
+                "data='" + data + '\'' +
+                ", format='" + format + '\'' +
+                '}';
+    }
+}

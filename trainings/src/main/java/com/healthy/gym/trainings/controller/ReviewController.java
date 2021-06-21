4b1064f8e22d8b78af0225ef7fdf90634790e919
bartoszkordek <bartosz.kordek10@gmail.com>
2021-06-22 01:32:26 +0200

    review controller updates

--- trainings/src/main/java/com/healthy/gym/trainings/controller/ReviewController.java
+++ trainings/src/main/java/com/healthy/gym/trainings/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package com.healthy.gym.trainings.controller;
 
+import com.healthy.gym.trainings.component.Translator;
 import com.healthy.gym.trainings.data.document.GroupTrainingsReviews;
 import com.healthy.gym.trainings.exception.*;
 import com.healthy.gym.trainings.model.request.GroupTrainingReviewRequest;
@@ -11,13 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,12 +28,14 @@ import java.util.Map;
 @RequestMapping("/review")
 public class ReviewController {
 
+    private final Translator translator;
     private final GroupTrainingService groupTrainingsService;
     private final ReviewService reviewService;
 
     @Autowired
-    public ReviewController(GroupTrainingService groupTrainingsService,
+    public ReviewController(Translator translator, GroupTrainingService groupTrainingsService,
                             ReviewService reviewService) {
+        this.translator = translator;
         this.groupTrainingsService = groupTrainingsService;
         this.reviewService = reviewService;
     }
@@ -54,22 +58,22 @@ public class ReviewController {
     public GroupTrainingReviewResponse createGroupTrainingReview(
             @Valid @RequestBody GroupTrainingReviewRequest groupTrainingsReviews,
             @RequestParam final String clientId
-    ) throws RestException {
+    ) {
         try {
             return reviewService.createGroupTrainingReview(groupTrainingsReviews, clientId);
         } catch (StarsOutOfRangeException e) {
-            throw new RestException(e.getMessage(), HttpStatus.BAD_REQUEST, e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
     }
 
 
-    //TODO for admin paginacja, filtrowanie po datach
+    //TODO for admin
     @GetMapping("/page/{page}")
     public ResponseEntity<Map<String, Object>> getAllReviews(
-            @RequestParam(required = false) final String startDate,
-            @RequestParam(required = false) final String endDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") final String startDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") final String endDate,
             @RequestParam(defaultValue = "10") final int size,
-            @PathVariable final int page) throws RestException {
+            @PathVariable final int page) {
 
         Pageable paging = PageRequest.of(page, size);
 
@@ -81,7 +85,7 @@ public class ReviewController {
             return new ResponseEntity<>(response, HttpStatus.OK);
 
         } catch (ParseException | StartDateAfterEndDateException e){
-            throw new RestException(e.getMessage(), HttpStatus.BAD_REQUEST, e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
 
     }
@@ -89,11 +93,11 @@ public class ReviewController {
     // TODO only for admin and user who owns it
     @GetMapping("/user/{userId}/page/{page}")
     public ResponseEntity<Map<String, Object>> getAllReviewsByUserId(
-            @RequestParam(required = false) final String startDate,
-            @RequestParam(required = false) final String endDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") final String startDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") final String endDate,
             @RequestParam(defaultValue = "10") final int size,
             @PathVariable final String userId,
-            @PathVariable final int page) throws RestException {
+            @PathVariable final int page) {
 
         Pageable paging = PageRequest.of(page, size);
 
@@ -104,15 +108,15 @@ public class ReviewController {
 
             return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (ParseException | StartDateAfterEndDateException e){
-            throw new RestException(e.getMessage(), HttpStatus.BAD_REQUEST, e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
     }
 
     // TODO only logged in users
     @GetMapping("/trainingType/{trainingTypeId}/page/{page}")
     public ResponseEntity<Map<String, Object>> getAllReviewsByTrainingTypeId(
-            @RequestParam(required = false) final String startDate,
-            @RequestParam(required = false) final String endDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") final String startDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") final String endDate,
             @RequestParam(defaultValue = "10") final int size,
             @PathVariable final String trainingTypeId,
             @PathVariable final int page) throws RestException {
@@ -126,7 +130,7 @@ public class ReviewController {
 
             return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (ParseException | StartDateAfterEndDateException e){
-            throw new RestException(e.getMessage(), HttpStatus.BAD_REQUEST, e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
     }
 
@@ -139,11 +143,11 @@ public class ReviewController {
     @GetMapping("/{reviewId}")
     public GroupTrainingsReviews getGroupTrainingReviewById(
             @PathVariable("reviewId") final String reviewId
-    ) throws RestException {
+    ) {
         try {
             return groupTrainingsService.getGroupTrainingReviewById(reviewId);
         } catch (NotExistingGroupTrainingReviewException e) {
-            throw new RestException(e.getMessage(), HttpStatus.BAD_REQUEST, e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
     }
 
@@ -153,13 +157,14 @@ public class ReviewController {
             @Valid @RequestBody final GroupTrainingReviewUpdateRequest groupTrainingsReviewsUpdateModel,
             @PathVariable("reviewId") final String reviewId,
             @RequestParam final String clientId
-    ) throws RestException {
+    ) {
         try {
             return groupTrainingsService.updateGroupTrainingReview(groupTrainingsReviewsUpdateModel, reviewId, clientId);
         } catch (NotExistingGroupTrainingReviewException | StarsOutOfRangeException e) {
-            throw new RestException(e.getMessage(), HttpStatus.BAD_REQUEST, e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         } catch (NotAuthorizedClientException e) {
-            throw new RestException(e.getMessage(), HttpStatus.FORBIDDEN, e);
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+
         }
     }
 
@@ -168,13 +173,13 @@ public class ReviewController {
     public GroupTrainingsReviews removeGroupTrainingReview(
             @PathVariable("reviewId") final String reviewId,
             @RequestParam final String clientId
-    ) throws RestException {
+    ) {
         try {
             return groupTrainingsService.removeGroupTrainingReview(reviewId, clientId);
         } catch (NotExistingGroupTrainingReviewException e) {
-            throw new RestException(e.getMessage(), HttpStatus.BAD_REQUEST, e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         } catch (NotAuthorizedClientException e) {
-            throw new RestException(e.getMessage(), HttpStatus.FORBIDDEN, e);
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
         }
     }
 }

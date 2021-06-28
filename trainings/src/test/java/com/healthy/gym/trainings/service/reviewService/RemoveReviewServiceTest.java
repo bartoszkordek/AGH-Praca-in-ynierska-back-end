c152bc0699d64b4b49d3e2a330d5bf26938ef193
bartoszkordek <bartosz.kordek10@gmail.com>
2021-06-28 23:20:24 +0200

    Remove Group Training Review - reviewId and clientId validation

--- trainings/src/test/java/com/healthy/gym/trainings/service/reviewService/RemoveReviewServiceTest.java
+++ trainings/src/test/java/com/healthy/gym/trainings/service/reviewService/RemoveReviewServiceTest.java
@@ -13,12 +13,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
@@ -71,6 +66,90 @@ public class RemoveReviewServiceTest {
         //then
         assertThat(reviewService.removeGroupTrainingReviewByReviewId(reviewId, clientId))
                 .isEqualTo(reviewResponse);
+    }
+
+    @Test(expected = NotExistingGroupTrainingReviewException.class)
+    public void shouldNotRemoveReview_whenInvalidReviewId() throws NotAuthorizedClientException, NotExistingGroupTrainingReviewException {
+        //mocks
+        ReviewDAO reviewRepository = Mockito.mock(ReviewDAO.class);
+        TrainingTypeDAO trainingTypeRepository = Mockito.mock(TrainingTypeDAO.class);
+        ReviewService reviewService = new ReviewServiceImpl(reviewRepository, trainingTypeRepository);
+
+        //before
+        String reviewId = "852ed953-e37f-435a-bd1e-9fb2a327c4d5";
+        String trainingName = "TestTrainingName";
+        String clientId = "Client123";
+        String date = "2021-01-01";
+        int stars = 5;
+        String text = "Very good training!";
+
+        GroupTrainingsReviews reviewToRemove = new GroupTrainingsReviews(
+                reviewId,
+                trainingName,
+                clientId,
+                date,
+                stars,
+                text
+        );
+        GroupTrainingReviewResponse reviewResponse = new GroupTrainingReviewResponse(
+                reviewId,
+                trainingName,
+                clientId,
+                date,
+                stars,
+                text);
+
+        //when
+        when(reviewRepository.existsByReviewId(reviewId))
+                .thenReturn(false);
+        when(reviewRepository.existsByReviewIdAndAndClientId(reviewId, clientId))
+                .thenReturn(false);
+        when(reviewRepository.findGroupTrainingsReviewsByReviewId(reviewId))
+                .thenReturn(reviewToRemove);
+
+        reviewService.removeGroupTrainingReviewByReviewId(reviewId, clientId);
+    }
+
+    @Test(expected = NotAuthorizedClientException.class)
+    public void shouldNotRemoveReview_whenClientIsNotOwnerOfReview() throws NotAuthorizedClientException, NotExistingGroupTrainingReviewException {
+        //mocks
+        ReviewDAO reviewRepository = Mockito.mock(ReviewDAO.class);
+        TrainingTypeDAO trainingTypeRepository = Mockito.mock(TrainingTypeDAO.class);
+        ReviewService reviewService = new ReviewServiceImpl(reviewRepository, trainingTypeRepository);
+
+        //before
+        String reviewId = "852ed953-e37f-435a-bd1e-9fb2a327c4d5";
+        String trainingName = "TestTrainingName";
+        String clientId = "Client123";
+        String date = "2021-01-01";
+        int stars = 5;
+        String text = "Very good training!";
+
+        GroupTrainingsReviews reviewToRemove = new GroupTrainingsReviews(
+                reviewId,
+                trainingName,
+                clientId,
+                date,
+                stars,
+                text
+        );
+        GroupTrainingReviewResponse reviewResponse = new GroupTrainingReviewResponse(
+                reviewId,
+                trainingName,
+                clientId,
+                date,
+                stars,
+                text);
+
+        //when
+        when(reviewRepository.existsByReviewId(reviewId))
+                .thenReturn(true);
+        when(reviewRepository.existsByReviewIdAndAndClientId(reviewId, clientId))
+                .thenReturn(false);
+        when(reviewRepository.findGroupTrainingsReviewsByReviewId(reviewId))
+                .thenReturn(reviewToRemove);
+
+        reviewService.removeGroupTrainingReviewByReviewId(reviewId, clientId);
     }
 
 

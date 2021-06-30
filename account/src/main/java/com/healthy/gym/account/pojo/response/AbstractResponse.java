e87e17a82c0ea907542ee7b6ed05ac25343174b9
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-30 21:51:25 +0200

    Add field errors to account AbstractResponse.

--- account/src/main/java/com/healthy/gym/account/pojo/response/AbstractResponse.java
+++ account/src/main/java/com/healthy/gym/account/pojo/response/AbstractResponse.java
@@ -1,18 +1,27 @@
 package com.healthy.gym.account.pojo.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.io.Serializable;
 import java.util.Map;
 import java.util.Objects;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public abstract class AbstractResponse implements Serializable {
 
     private String message;
+    private Map<String, String> errors;
 
     protected AbstractResponse() {
     }
 
     protected AbstractResponse(String message) {
         this.message = message;
+    }
+
+    protected AbstractResponse(String message, Map<String, String> errors) {
+        this.message = message;
+        this.errors = errors;
     }
 
     public String getMessage() {
@@ -23,23 +32,33 @@ public abstract class AbstractResponse implements Serializable {
         this.message = message;
     }
 
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Map<String, String> errors) {
+        this.errors = errors;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         AbstractResponse that = (AbstractResponse) o;
-        return Objects.equals(message, that.message);
+        return Objects.equals(message, that.message)
+                && Objects.equals(errors, that.errors);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(message);
+        return Objects.hash(message, errors);
     }
 
     @Override
     public String toString() {
         return "AbstractResponse{" +
                 "message='" + message + '\'' +
+                ", errors=" + errors +
                 '}';
     }
 }

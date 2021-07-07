b55cd349c0f0d65428326a63525a4a979adbcd94
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-07 22:36:17 +0200

    Add integration test for deleteAvatar().

---
+++ account/src/test/java/com/healthy/gym/account/controller/photoController/integrationTest/WhenDeleteAvatarIntegrationTest.java
@@ -0,0 +1,180 @@
+package com.healthy.gym.account.controller.photoController.integrationTest;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.healthy.gym.account.component.TokenManager;
+import com.healthy.gym.account.configuration.tests.TestCountry;
+import com.healthy.gym.account.configuration.tests.TestRoleTokenFactory;
+import com.healthy.gym.account.data.document.PhotoDocument;
+import com.healthy.gym.account.data.document.UserDocument;
+import com.healthy.gym.account.pojo.Image;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.healthy.gym.account.configuration.tests.LocaleConverter.convertEnumToLocale;
+import static com.healthy.gym.account.configuration.tests.Messages.getMessagesAccordingToLocale;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+        "eureka.client.fetch-registry=false",
+        "eureka.client.register-with-eureka=false"
+})
+class WhenDeleteAvatarIntegrationTest {
+
+    @Container
+    static MongoDBContainer mongoDBContainer =
+            new MongoDBContainer(DockerImageName.parse("mongo:4.4.4-bionic"));
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Autowired
+    private TokenManager tokenManager;
+    @Autowired
+    private TestRoleTokenFactory tokenFactory;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private String userToken;
+    private String userId;
+    private String adminToken;
+
+    @LocalServerPort
+    private Integer port;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @BeforeEach
+    void setUp() throws IOException {
+        userId = UUID.randomUUID().toString();
+        userToken = tokenFactory.getUserToken(userId);
+
+        String adminId = UUID.randomUUID().toString();
+        adminToken = tokenFactory.getAdminToken(adminId);
+
+        Resource currentImageResource = new ClassPathResource("testImagess/shiba_inu_smile_1.jpg");
+
+        PhotoDocument avatar = new PhotoDocument(
+                userId,
+                currentImageResource.getFilename(),
+                new Image(getImageBytes(currentImageResource), MediaType.IMAGE_JPEG_VALUE)
+        );
+
+        UserDocument userDocument = new UserDocument(
+                "testName",
+                "testSurname",
+                "testEmail",
+                "testPhone",
+                "encryptedPassword",
+                userId
+        );
+
+        mongoTemplate.save(userDocument);
+        mongoTemplate.save(avatar);
+    }
+
+    @AfterEach
+    void tearDown() {
+        mongoTemplate.dropCollection(UserDocument.class);
+        mongoTemplate.dropCollection(PhotoDocument.class);
+    }
+
+    private byte[] getImageBytes(Resource imageResource) throws IOException {
+        File imageFile = imageResource.getFile();
+        FileInputStream inputStream = new FileInputStream(imageFile);
+        return inputStream.readAllBytes();
+    }
+
+    @ParameterizedTest
+    @EnumSource(TestCountry.class)
+    void shouldAcceptRequestAndShouldReturnAvatar(TestCountry country) throws Exception {
+        List<PhotoDocument> avatars = mongoTemplate.findAll(PhotoDocument.class);
+        assertThat(avatars.size()).isEqualTo(1);
+
+        Map<String, String> messages = getMessagesAccordingToLocale(country);
+        Locale testedLocale = convertEnumToLocale(country);
+
+        URI uri = new URI("http://localhost:" + port + "/photos/" + userId + "/avatar");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept-Language", testedLocale.toString());
+        headers.set("Authorization", userToken);
+        headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+
+        HttpEntity<Object> requestEntity = new HttpEntity<>(null, headers);
+        String expectedMessage = messages.get("avatar.removed");
+
+        ResponseEntity<JsonNode> responseEntity = restTemplate
+                .exchange(uri, HttpMethod.DELETE, requestEntity, JsonNode.class);
+
+        JsonNode responseBody = responseEntity.getBody();
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON);
+        assertThat(responseBody.get("message").textValue()).isEqualTo(expectedMessage);
+        assertThat(responseBody.get("avatar").isEmpty()).isTrue();
+
+        avatars = mongoTemplate.findAll(PhotoDocument.class);
+        assertThat(avatars.isEmpty()).isTrue();
+    }
+
+    @ParameterizedTest
+    @EnumSource(TestCountry.class)
+    void shouldAcceptRequestAndShouldThrowExceptionWhenAvatarNotFound(TestCountry country) throws Exception {
+        Map<String, String> messages = getMessagesAccordingToLocale(country);
+        Locale testedLocale = convertEnumToLocale(country);
+
+        URI uri = new URI("http://localhost:" + port + "/photos/" + UUID.randomUUID() + "/avatar");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept-Language", testedLocale.toString());
+        headers.set("Authorization", adminToken);
+        headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+
+        HttpEntity<Object> requestEntity = new HttpEntity<>(null, headers);
+        String expectedMessage = messages.get("avatar.not.found.exception");
+
+        ResponseEntity<JsonNode> responseEntity = restTemplate
+                .exchange(uri, HttpMethod.DELETE, requestEntity, JsonNode.class);
+
+        JsonNode responseBody = responseEntity.getBody();
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(responseEntity.getHeaders().getContentType()).hasToString(MediaType.APPLICATION_JSON_VALUE);
+        assertThat(responseBody.get("message").textValue()).isEqualTo(expectedMessage);
+        assertThat(responseBody.get("error").textValue()).isEqualTo("Not Found");
+        assertThat(responseBody.get("status").numberValue()).isEqualTo(404);
+        assertThat(responseBody.get("timestamp")).isNotNull();
+    }
+}

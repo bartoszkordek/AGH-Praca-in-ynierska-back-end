b4ef57a24b1f006a6c86fc133da299f4650050b3
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-30 13:29:54 +0200

    Refactor package name.

--- trainings/src/test/java/com/healthy/gym/trainings/controller/group/training/manager/unitTests/WhenCreateGroupTrainingTest.java
+++ trainings/src/test/java/com/healthy/gym/trainings/controller/group/training/manager/unitTests/WhenCreateGroupTrainingTest.java
@@ -1,4 +1,4 @@
-package com.healthy.gym.trainings.controller.group.training.groupTrainingManager;
+package com.healthy.gym.trainings.controller.group.training.manager.unitTests;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;

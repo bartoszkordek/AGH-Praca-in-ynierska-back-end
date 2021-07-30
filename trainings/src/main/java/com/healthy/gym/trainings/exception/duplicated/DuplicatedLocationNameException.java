fddc2a62b123d5c309f174a16d4353f5f35ca191
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-30 08:26:11 +0200

    Add new exception: DuplicatedLocationNameException.

---
+++ trainings/src/main/java/com/healthy/gym/trainings/exception/duplicated/DuplicatedLocationNameException.java
@@ -0,0 +1,11 @@
+package com.healthy.gym.trainings.exception.duplicated;
+
+public class DuplicatedLocationNameException extends Exception {
+
+    public DuplicatedLocationNameException() {
+    }
+
+    public DuplicatedLocationNameException(String message) {
+        super(message);
+    }
+}
--- trainings/src/main/java/com/healthy/gym/trainings/service/LocationService.java
+++ trainings/src/main/java/com/healthy/gym/trainings/service/LocationService.java
@@ -1,12 +1,13 @@
 package com.healthy.gym.trainings.service;
 
+import com.healthy.gym.trainings.exception.duplicated.DuplicatedLocationNameException;
 import com.healthy.gym.trainings.model.request.CreateLocationRequest;
 import com.healthy.gym.trainings.shared.LocationDTO;
 
 import java.util.List;
 
 public interface LocationService {
-    LocationDTO createLocation(CreateLocationRequest request);
+    LocationDTO createLocation(CreateLocationRequest request) throws DuplicatedLocationNameException;
 
     List<LocationDTO> getAllLocations();
 

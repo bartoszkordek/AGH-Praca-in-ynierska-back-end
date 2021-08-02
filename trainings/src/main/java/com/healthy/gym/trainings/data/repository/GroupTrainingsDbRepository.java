a1f98037ef3fc932a14083b86317666ee2ac36ac
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-08-02 07:28:32 +0200

    Add GroupTrainingsDbRepository interface.

---
+++ trainings/src/main/java/com/healthy/gym/trainings/data/repository/GroupTrainingsDbRepository.java
@@ -0,0 +1,71 @@
+package com.healthy.gym.trainings.data.repository;
+
+import com.healthy.gym.trainings.data.document.GroupTrainings;
+import com.healthy.gym.trainings.exception.StartDateAfterEndDateException;
+import com.healthy.gym.trainings.exception.invalid.InvalidDateException;
+import com.healthy.gym.trainings.exception.invalid.InvalidHourException;
+import com.healthy.gym.trainings.model.request.GroupTrainingRequest;
+import com.healthy.gym.trainings.model.response.GroupTrainingPublicResponse;
+import com.healthy.gym.trainings.model.response.GroupTrainingResponse;
+import com.healthy.gym.trainings.model.response.ParticipantsResponse;
+
+import java.text.ParseException;
+import java.util.List;
+
+public interface GroupTrainingsDbRepository {
+
+    List<GroupTrainingResponse> getGroupTrainings(String startDate, String endDate)
+            throws InvalidHourException, StartDateAfterEndDateException, ParseException, InvalidDateException;
+
+    List<GroupTrainingPublicResponse> getPublicGroupTrainings(String startDate, String endDate)
+            throws InvalidHourException, InvalidDateException, StartDateAfterEndDateException, ParseException;
+
+    GroupTrainingResponse getGroupTrainingById(String trainingId)
+            throws InvalidHourException, InvalidDateException;
+
+    List<GroupTrainingResponse> getGroupTrainingsByTrainingTypeId(
+            String trainingTypeId,
+            String startDate,
+            String endDate
+    ) throws ParseException, StartDateAfterEndDateException, InvalidDateException, InvalidHourException;
+
+    List<GroupTrainingPublicResponse> getGroupTrainingsPublicByTrainingTypeId(
+            String trainingTypeId,
+            String startDate,
+            String endDate
+    ) throws ParseException, StartDateAfterEndDateException, InvalidDateException, InvalidHourException;
+
+    List<GroupTrainingPublicResponse> getMyAllGroupTrainings(String clientId)
+            throws InvalidDateException, InvalidHourException;
+
+    List<ParticipantsResponse> getTrainingParticipants(String trainingId);
+
+    boolean isGroupTrainingExist(String trainingId);
+
+    boolean isGroupTrainingExistByType(String trainingTypeId);
+
+    boolean isAbilityToGroupTrainingEnrollment(String trainingId);
+
+    boolean isClientAlreadyEnrolledToGroupTraining(String trainingId, String clientId);
+
+    boolean isClientAlreadyExistInReserveList(String trainingId, String clientId);
+
+    void enrollToGroupTraining(String trainingId, String clientId);
+
+    void addToReserveList(String trainingId, String clientId);
+
+    void removeFromParticipants(String trainingId, String clientId);
+
+    void removeFromReserveList(String trainingId, String clientId);
+
+    boolean isAbilityToCreateTraining(GroupTrainingRequest groupTrainingModel);
+
+    boolean isAbilityToUpdateTraining(String trainingId, GroupTrainingRequest groupTrainingModel);
+
+    GroupTrainings createTraining(GroupTrainingRequest groupTrainingModel) throws InvalidHourException;
+
+    GroupTrainings removeTraining(String trainingId);
+
+    GroupTrainings updateTraining(String trainingId, GroupTrainingRequest groupTrainingModelRequest)
+            throws InvalidHourException;
+}

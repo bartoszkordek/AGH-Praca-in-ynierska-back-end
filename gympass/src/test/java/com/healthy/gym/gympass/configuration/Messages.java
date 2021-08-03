cd8380b98a0a7332ae7c01a14cd400e8e580469b
bartoszkordek <bartosz.kordek10@gmail.com>
2021-08-03 21:35:15 +0200

    integration test init schema

---
+++ gympass/src/test/java/com/healthy/gym/gympass/configuration/Messages.java
@@ -0,0 +1,25 @@
+package com.healthy.gym.gympass.configuration;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Messages {
+    private static Map<String, String> getMessagesPL() {
+        return Stream.of(new String[][]{
+                {"exception.internal.error", "Podczas przetwarzania żądania wystąpił błąd."}
+
+        }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
+    }
+
+    private static Map<String, String> getMessagesEN() {
+        return Stream.of(new String[][]{
+                {"exception.internal.error", "An error occurred while processing your request."}
+        }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
+    }
+
+    public static Map<String, String> getMessagesAccordingToLocale(TestCountry country) {
+        if (country == TestCountry.POLAND) return getMessagesPL();
+        return getMessagesEN();
+    }
+}

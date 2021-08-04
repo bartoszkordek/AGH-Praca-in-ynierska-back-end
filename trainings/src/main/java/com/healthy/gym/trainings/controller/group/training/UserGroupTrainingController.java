7088d1b46510c0ee645ee6a75db4c8129c2446cf
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-08-04 18:01:28 +0200

    Remove addToReserveList() and fix enrollToGroupTraining() in UserGroupTrainingController. Add proper tests.

--- trainings/src/main/java/com/healthy/gym/trainings/controller/group/training/UserGroupTrainingController.java
+++ trainings/src/main/java/com/healthy/gym/trainings/controller/group/training/UserGroupTrainingController.java
@@ -1,12 +1,15 @@
 package com.healthy.gym.trainings.controller.group.training;
 
 import com.healthy.gym.trainings.component.Translator;
-import com.healthy.gym.trainings.exception.training.TrainingEnrollmentException;
+import com.healthy.gym.trainings.exception.PastDateException;
+import com.healthy.gym.trainings.exception.UserAlreadyEnrolledToTrainingException;
 import com.healthy.gym.trainings.exception.notexisting.NotExistingGroupTrainingException;
-import com.healthy.gym.trainings.model.response.GroupTrainingEnrollmentResponse;
-import com.healthy.gym.trainings.shared.GroupTrainingEnrollmentDTO;
+import com.healthy.gym.trainings.exception.notfound.UserNotFoundException;
+import com.healthy.gym.trainings.exception.training.TrainingEnrollmentException;
 import com.healthy.gym.trainings.model.response.GroupTrainingPublicResponse;
+import com.healthy.gym.trainings.model.response.GroupTrainingResponse;
 import com.healthy.gym.trainings.service.group.training.UserGroupTrainingService;
+import com.healthy.gym.trainings.shared.GroupTrainingDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+
+import static com.healthy.gym.trainings.utils.ParticipantsExtractor.userIsInBasicList;
 
 @RestController
 @RequestMapping("/group")
@@ -50,23 +55,34 @@ public class UserGroupTrainingController {
 
     @PreAuthorize("hasRole('ADMIN') or hasRole('EMPLOYEE') or principal==#userId")
     @PostMapping("/{trainingId}/enroll")
-    public ResponseEntity<GroupTrainingEnrollmentResponse> enrollToGroupTraining(
+    public ResponseEntity<GroupTrainingResponse> enrollToGroupTraining(
             @PathVariable("trainingId") final String trainingId,
             @RequestParam("clientId") final String userId
     ) {
         try {
-            String message = translator.toLocale("enrollment.success");
-            GroupTrainingEnrollmentDTO enrolledTraining
+            GroupTrainingDTO enrolledTraining
                     = userGroupTrainingService.enrollToGroupTraining(trainingId, userId);
+            String message = getProperMessage(enrolledTraining, userId);
 
             return ResponseEntity
                     .status(HttpStatus.OK)
-                    .body(new GroupTrainingEnrollmentResponse(message, enrolledTraining)
-                    );
+                    .body(new GroupTrainingResponse(message, enrolledTraining));
 
-        } catch (TrainingEnrollmentException e) {
-            String reason = translator.toLocale(EXCEPTION_GROUP_TRAINING_ENROLLMENT);
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, reason, e);
+        } catch (NotExistingGroupTrainingException exception) {
+            String reason = translator.toLocale("exception.group.training.not.found");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, reason, exception);
+
+        } catch (PastDateException exception) {
+            String reason = translator.toLocale("exception.past.date.enrollment");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, reason, exception);
+
+        } catch (UserNotFoundException exception) {
+            String reason = translator.toLocale("exception.not.found.user.id");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, reason, exception);
+
+        } catch (UserAlreadyEnrolledToTrainingException exception) {
+            String reason = translator.toLocale("exception.user.already.enrolled.to.training");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, reason, exception);
 
         } catch (Exception exception) {
             String reason = translator.toLocale(EXCEPTION_INTERNAL_ERROR);
@@ -75,28 +91,9 @@ public class UserGroupTrainingController {
         }
     }
 
-    @PreAuthorize("hasRole('ADMIN') or hasRole('EMPLOYEE') or principal==#userId")
-    @PostMapping("/{trainingId}/reservelist/add")
-    public void addToReserveList(
-            @PathVariable("trainingId") final String trainingId,
-            @RequestParam("clientId") final String userId
-    ) {
-        try {
-            userGroupTrainingService.addToReserveList(trainingId, userId);
-
-        } catch (NotExistingGroupTrainingException e) {
-            String reason = translator.toLocale(EXCEPTION_NOT_FOUND_TRAINING_ID);
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, reason, e);
-
-        } catch (TrainingEnrollmentException e) {
-            String reason = translator.toLocale(EXCEPTION_GROUP_TRAINING_ENROLLMENT);
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, reason, e);
-
-        } catch (Exception exception) {
-            String reason = translator.toLocale(EXCEPTION_INTERNAL_ERROR);
-            exception.printStackTrace();
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, reason, exception);
-        }
+    private String getProperMessage(GroupTrainingDTO enrolledTraining, String userId) {
+        if (userIsInBasicList(enrolledTraining, userId)) return translator.toLocale("enrollment.success.basic.list");
+        return translator.toLocale("enrollment.success.reserve.list");
     }
 
     @PreAuthorize("hasRole('ADMIN') or hasRole('EMPLOYEE') or principal==#userId")

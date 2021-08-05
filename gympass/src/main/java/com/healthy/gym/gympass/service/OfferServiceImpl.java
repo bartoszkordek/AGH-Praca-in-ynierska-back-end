8782231af600f9288d1785ed942f824e666499df
bartoszkordek <bartosz.kordek10@gmail.com>
2021-08-05 14:35:48 +0200

    connect GymPassOfferDAO to OfferServiceImpl

--- gympass/src/main/java/com/healthy/gym/gympass/service/OfferServiceImpl.java
+++ gympass/src/main/java/com/healthy/gym/gympass/service/OfferServiceImpl.java
@@ -1,16 +1,32 @@
 package com.healthy.gym.gympass.service;
 
+import com.healthy.gym.gympass.data.repository.GymPassOfferDAO;
 import com.healthy.gym.gympass.dto.GymPassDTO;
 import com.healthy.gym.gympass.exception.DuplicatedOffersException;
 import com.healthy.gym.gympass.exception.InvalidGymPassOfferId;
 import com.healthy.gym.gympass.exception.NoOffersException;
 import com.healthy.gym.gympass.pojo.request.GymPassOfferRequest;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class OfferServiceImpl implements OfferService{
+
+    private final GymPassOfferDAO gymPassOfferDAO;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public OfferServiceImpl(
+            GymPassOfferDAO gymPassOfferDAO
+    ){
+        this.gymPassOfferDAO = gymPassOfferDAO;
+        modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    }
 
     @Override
     public List<GymPassDTO> getGymPassOffer()

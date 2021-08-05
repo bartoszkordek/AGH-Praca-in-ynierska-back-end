22166ba7d54c65d6340336d05bc8e133b4fd8664
bartoszkordek <bartosz.kordek10@gmail.com>
2021-08-05 14:33:08 +0200

    GymPassOfferDAO init

---
+++ gympass/src/main/java/com/healthy/gym/gympass/data/repository/GymPassOfferDAO.java
@@ -0,0 +1,9 @@
+package com.healthy.gym.gympass.data.repository;
+
+import com.healthy.gym.gympass.data.document.GymPassDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface GymPassOfferDAO extends MongoRepository<GymPassDocument, String> {
+
+
+}

49bcca26a1323d194459e0abd185256b668bc01a
bartoszkordek <bartosz.kordek10@gmail.com>
2021-08-08 16:33:05 +0200

    ShouldNotCreateOfferWhenNotAuthorized integration tests

--- gympass/src/test/java/com/healthy/gym/gympass/controller/integrationTests/purchase/PurchaseGymPassIntegrationTest.java
+++ gympass/src/test/java/com/healthy/gym/gympass/controller/integrationTests/purchase/PurchaseGymPassIntegrationTest.java
@@ -263,6 +263,59 @@ public class PurchaseGymPassIntegrationTest {
     @Nested
     class ShouldNotPurchaseGymPass{
 
+        @Nested
+        class ShouldNotCreateOfferWhenNotAuthorized{
 
+            @ParameterizedTest
+            @EnumSource(TestCountry.class)
+            void shouldNotGetOffersWhenNoToken(TestCountry country) throws Exception {
+                Locale testedLocale = convertEnumToLocale(country);
+
+                URI uri = new URI("http://localhost:" + port + "/purchase");
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.set("Accept-Language", testedLocale.toString());
+                headers.setContentType(MediaType.APPLICATION_JSON);
+
+                HttpEntity<Object> request = new HttpEntity<>(timePurchasedGymPassRequestContent, headers);
+
+                ResponseEntity<JsonNode> responseEntity = restTemplate
+                        .exchange(uri, HttpMethod.POST, request, JsonNode.class);
+
+
+                assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+                assertThat(responseEntity.getBody().get("status").intValue()).isEqualTo(403);
+                assertThat(responseEntity.getBody().get("error").textValue()).isEqualTo("Forbidden");
+                assertThat(responseEntity.getBody().get("message").textValue()).isEqualTo("Access Denied");
+                assertThat(responseEntity.getBody().get("timestamp")).isNotNull();
+            }
+
+            @ParameterizedTest
+            @EnumSource(TestCountry.class)
+            void shouldNotGetOffersWhenLoggedAsUser(TestCountry country) throws Exception {
+                Map<String, String> messages = getMessagesAccordingToLocale(country);
+                Locale testedLocale = convertEnumToLocale(country);
+
+                URI uri = new URI("http://localhost:" + port + "/purchase");
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.set("Accept-Language", testedLocale.toString());
+                headers.set("Authorization", userToken);
+                headers.setContentType(MediaType.APPLICATION_JSON);
+
+                HttpEntity<Object> request = new HttpEntity<>(timePurchasedGymPassRequestContent, headers);
+
+                ResponseEntity<JsonNode> responseEntity = restTemplate
+                        .exchange(uri, HttpMethod.POST, request, JsonNode.class);
+
+                String expectedMessage = messages.get("exception.access.denied");
+
+                assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+                assertThat(responseEntity.getBody().get("status").intValue()).isEqualTo(403);
+                assertThat(responseEntity.getBody().get("error").textValue()).isEqualTo("Forbidden");
+                assertThat(responseEntity.getBody().get("message").textValue()).isEqualTo(expectedMessage);
+                assertThat(responseEntity.getBody().get("timestamp")).isNotNull();
+            }
+        }
     }
 }

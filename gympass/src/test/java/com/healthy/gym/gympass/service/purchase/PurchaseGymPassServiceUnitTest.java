d97815fbbc9cabb5047bf33677912e4fa5f76260
bartoszkordek <bartosz.kordek10@gmail.com>
2021-08-08 14:07:43 +0200

    shouldNotPurchaseGymPass_whenNotSpecifiedGymPassType service test

--- gympass/src/test/java/com/healthy/gym/gympass/service/purchase/PurchaseGymPassServiceUnitTest.java
+++ gympass/src/test/java/com/healthy/gym/gympass/service/purchase/PurchaseGymPassServiceUnitTest.java
@@ -237,8 +237,6 @@ public class PurchaseGymPassServiceUnitTest {
         boolean isPremium = false;
         String name = "Jan";
         String surname = "kowalski";
-        LocalDateTime purchaseDateAndTime = LocalDateTime.now();
-
 
         //DB documents
         UserDocument userDocument = new UserDocument();
@@ -297,8 +295,6 @@ public class PurchaseGymPassServiceUnitTest {
         boolean isPremium = false;
         String name = "Jan";
         String surname = "kowalski";
-        LocalDateTime purchaseDateAndTime = LocalDateTime.now();
-
 
         //DB documents
         UserDocument userDocument = new UserDocument();
@@ -330,6 +326,65 @@ public class PurchaseGymPassServiceUnitTest {
         assertThatThrownBy(() ->
                 purchaseService.purchaseGymPass(purchasedGymPassRequest)
         ).isInstanceOf(StartDateAfterEndDateException.class);
+    }
+
+    @Test
+    void shouldNotPurchaseGymPass_whenNotSpecifiedGymPassType(){
+
+        //before
+        //request
+        String gymPassOfferId = UUID.randomUUID().toString();
+        String userId = UUID.randomUUID().toString();
+        String requestStartDate = LocalDate.now().plusMonths(1).format(DateTimeFormatter.ISO_DATE);
+        String requestEndDate = LocalDate.MAX.format(DateTimeFormatter.ISO_DATE);
+        int entries = Integer.MAX_VALUE;
+        PurchasedGymPassRequest purchasedGymPassRequest = new PurchasedGymPassRequest();
+        purchasedGymPassRequest.setGymPassOfferId(gymPassOfferId);
+        purchasedGymPassRequest.setUserId(userId);
+        purchasedGymPassRequest.setStartDate(requestStartDate);
+        purchasedGymPassRequest.setEndDate(requestEndDate);
+        purchasedGymPassRequest.setEntries(entries);
+
+        //response
+        String title = "Karnet miesięczny";
+        double amount = 139.99;
+        String currency = "zł";
+        String period = "miesiąc";
+        boolean isPremium = false;
+        String name = "Jan";
+        String surname = "kowalski";
+
+
+        //DB documents
+        UserDocument userDocument = new UserDocument();
+        userDocument.setName(name);
+        userDocument.setSurname(surname);
+        userDocument.setUserId(userId);
+        userDocument.setGymRoles(List.of(GymRole.USER));
+        userDocument.setId("507f1f77bcf86cd799435213");
+
+        String subheader = "Najepszy wybór dla regularnie uprawiających sport";
+        String synopsis = "Nielimitowana liczba wejść";
+        List<String> features = List.of("siłownia", "fitness", "TRX", "rowery");
+        GymPassDocument gymPassOfferDocument = new GymPassDocument(
+                gymPassOfferId,
+                title,
+                subheader,
+                new Price(amount, currency, period),
+                isPremium,
+                new Description(synopsis,features)
+        );
+        gymPassOfferDocument.setId("507f1f77bcf86cd799439011");
+
+
+        //when
+        when(gymPassOfferDAO.findByDocumentId(gymPassOfferId)).thenReturn(gymPassOfferDocument);
+        when(userDAO.findByUserId(userId)).thenReturn(userDocument);
+
+        //then
+        assertThatThrownBy(() ->
+                purchaseService.purchaseGymPass(purchasedGymPassRequest)
+        ).isInstanceOf(NotSpecifiedGymPassTypeException.class);
     }
 
 }

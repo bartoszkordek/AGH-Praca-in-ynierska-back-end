19b52172cd361bb5fd676f28bf1ef11eb63ce8f3
bartoszkordek <bartosz.kordek10@gmail.com>
2021-08-09 20:59:42 +0200

    shouldNotSuspendGymPassWhenRetroSuspensionDate integration test

--- gympass/src/test/java/com/healthy/gym/gympass/controller/integrationTests/purchase/SuspendPurchasedGymPassIntegrationTest.java
+++ gympass/src/test/java/com/healthy/gym/gympass/controller/integrationTests/purchase/SuspendPurchasedGymPassIntegrationTest.java
@@ -289,6 +289,36 @@ public class SuspendPurchasedGymPassIntegrationTest {
             assertThat(responseEntity.getBody().get("timestamp")).isNotNull();
         }
 
+
+        @ParameterizedTest
+        @EnumSource(TestCountry.class)
+        void shouldNotSuspendGymPassWhenRetroSuspensionDate(TestCountry country) throws URISyntaxException {
+            Map<String, String> messages = getMessagesAccordingToLocale(country);
+            Locale testedLocale = convertEnumToLocale(country);
+
+            String suspensionDate = LocalDate.now().minusDays(1).format(DateTimeFormatter.ISO_LOCAL_DATE);
+
+            URI uri = new URI("http://localhost:" + port + "/purchase/"+purchasedGymPassDocumentId+
+                    "/suspend/"+suspensionDate);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Accept-Language", testedLocale.toString());
+            headers.set("Authorization", employeeToken);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<Object> request = new HttpEntity<>(null, headers);
+
+            String expectedMessage = messages.get("exception.retro.date.suspension");
+
+            ResponseEntity<JsonNode> responseEntity = restTemplate
+                    .exchange(uri, HttpMethod.PUT, request, JsonNode.class);
+
+            assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            assertThat(Objects.requireNonNull(responseEntity.getBody().get("message").textValue()))
+                    .isEqualTo(expectedMessage);
+            assertThat(responseEntity.getBody().get("timestamp")).isNotNull();
+        }
+
         @Nested
         class ShouldNotSuspendGymPassWhenNotAuthorized{
 

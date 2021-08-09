b54cecf04288d37c17ae1021d3ac3fa621bc7472
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-08-09 19:11:59 +0200

    Remove useless methods and classes.

--- trainings/src/main/java/com/healthy/gym/trainings/data/repository/individual/training/IndividualTrainingRepository.java
+++ trainings/src/main/java/com/healthy/gym/trainings/data/repository/individual/training/IndividualTrainingRepository.java
@@ -3,15 +3,13 @@ package com.healthy.gym.trainings.data.repository.individual.training;
 import com.healthy.gym.trainings.data.document.IndividualTrainingDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface IndividualTrainingRepository extends MongoRepository<IndividualTrainingDocument, String> {
 
-//    List<IndividualTrainingDocument> findIndividualTrainingsByClientIdEquals(String clientId);
+    List<IndividualTrainingDocument> findAllByAccepted(boolean accepted);
 
-//    List<IndividualTrainingDocument> findAllByAccepted(boolean accepted);
-//
-//    IndividualTrainingDocument findIndividualTrainingsById(String trainingId);
-//
-////    boolean existsIndividualTrainingsByIdAndClientIdEquals(String trainingId, String clientId);
-//
-//    void deleteIndividualTrainingsById(String trainingId);
+    IndividualTrainingDocument findIndividualTrainingsById(String trainingId);
+
+    void deleteIndividualTrainingsById(String trainingId);
 }
--- trainings/src/main/java/com/healthy/gym/trainings/utils/GroupTrainingValidator.java
+++ trainings/src/main/java/com/healthy/gym/trainings/utils/GroupTrainingValidator.java
@@ -1,13 +1,7 @@
 package com.healthy.gym.trainings.utils;
 
-import com.healthy.gym.trainings.model.request.GroupTrainingRequest;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalTime;
-import java.util.Date;
-import java.util.List;
 
 public class GroupTrainingValidator {
 
@@ -15,36 +9,8 @@ public class GroupTrainingValidator {
         throw new IllegalStateException("Utility class");
     }
 
-    public static boolean isExistRequiredDataForGroupTraining(GroupTrainingRequest groupTrainingModel) {
-        String trainingName = groupTrainingModel.getTrainingTypeId();
-        List<String> trainers = groupTrainingModel.getTrainers();
-        String date = groupTrainingModel.getDate();
-        String startTime = groupTrainingModel.getStartTime();
-        String endTime = groupTrainingModel.getEndTime();
-
-        return !trainingName.isEmpty()
-                && !trainers.isEmpty()
-                && !date.isEmpty()
-                && !startTime.isEmpty()
-                && !endTime.isEmpty();
-    }
-
     public static boolean isHallNoInvalid(int hallNo) {
         return hallNo <= 0;
-    }
-
-    public static boolean isLimitInvalid(int limit) {
-        return limit <= 0;
-    }
-
-    public static boolean isTrainingRetroDate(String date) throws ParseException {
-        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");
-        Date requestDateParsed = sdfDate.parse(date);
-        Date now = new Date();
-        String todayDateFormatted = sdfDate.format(now);
-        Date todayDateParsed = sdfDate.parse(todayDateFormatted);
-
-        return requestDateParsed.before(todayDateParsed);
     }
 
     public static boolean isStartTimeAfterEndTime(String startTime, String endTime) {

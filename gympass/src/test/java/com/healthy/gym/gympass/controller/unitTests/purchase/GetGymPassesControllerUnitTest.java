d3417f4456122a8c0ef87c20ae0fda3c8b8bd409
bartoszkordek <bartosz.kordek10@gmail.com>
2021-08-12 02:06:20 +0200

    shouldThrowIllegalStateExceptionWhenInternalErrorOccurs controller unit tests

--- gympass/src/test/java/com/healthy/gym/gympass/controller/unitTests/purchase/GetGymPassesControllerUnitTest.java
+++ gympass/src/test/java/com/healthy/gym/gympass/controller/unitTests/purchase/GetGymPassesControllerUnitTest.java
@@ -344,5 +344,36 @@ public class GetGymPassesControllerUnitTest {
             }
 
         }
+
+        @ParameterizedTest
+        @EnumSource(TestCountry.class)
+        void shouldThrowIllegalStateExceptionWhenInternalErrorOccurs(TestCountry country)
+                throws Exception {
+            Map<String, String> messages = getMessagesAccordingToLocale(country);
+            Locale testedLocale = convertEnumToLocale(country);
+
+            int page = 0;
+
+            RequestBuilder request = MockMvcRequestBuilders
+                    .get(uri+"/page/"+page)
+                    .header("Accept-Language", testedLocale.toString())
+                    .header("Authorization", adminToken)
+                    .contentType(MediaType.APPLICATION_JSON);
+
+            doThrow(IllegalStateException.class)
+                    .when(purchaseService)
+                    .getGymPasses(any(), any(), any());
+
+            String expectedMessage = messages.get("exception.internal.error");
+
+            mockMvc.perform(request)
+                    .andDo(print())
+                    .andExpect(status().isInternalServerError())
+                    .andExpect(status().reason(is(expectedMessage)))
+                    .andExpect(result ->
+                            assertThat(Objects.requireNonNull(result.getResolvedException()).getCause())
+                                    .isInstanceOf(IllegalStateException.class)
+                    );
+        }
     }
 }

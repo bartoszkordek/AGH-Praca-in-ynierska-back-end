95f85247a88578c31303aed7e6804c5fe2ffb402
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-08-12 10:06:17 +0200

    Add findAllByStartDateTimeIsAfterAndEndDateTimeIsBefore method to DAO interfaces.

--- trainings/src/main/java/com/healthy/gym/trainings/data/repository/group/training/GroupTrainingsDAO.java
+++ trainings/src/main/java/com/healthy/gym/trainings/data/repository/group/training/GroupTrainingsDAO.java
@@ -1,9 +1,17 @@
 package com.healthy.gym.trainings.data.repository.group.training;
 
 import com.healthy.gym.trainings.data.document.GroupTrainingDocument;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface GroupTrainingsDAO extends MongoRepository<GroupTrainingDocument, String> {
 
     GroupTrainingDocument findFirstByGroupTrainingId(String trainingId);
+
+    List<GroupTrainingDocument> findAllByStartDateIsAfterAndEndDateIsBefore(
+            LocalDateTime startDateTime, LocalDateTime endDateTime, Sort sort
+    );
 }

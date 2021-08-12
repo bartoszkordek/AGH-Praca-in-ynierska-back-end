fc3f77a4e0593094020535bf9347ca724a988bc4
bartoszkordek <bartosz.kordek10@gmail.com>
2021-08-12 03:19:50 +0200

    shouldNotGetGymPasses_whenStartDateAfterEndDate integration test

--- gympass/src/test/java/com/healthy/gym/gympass/controller/integrationTests/purchase/GetGymPassesIntegrationTest.java
+++ gympass/src/test/java/com/healthy/gym/gympass/controller/integrationTests/purchase/GetGymPassesIntegrationTest.java
@@ -530,6 +530,39 @@ public class GetGymPassesIntegrationTest {
         }
     }
 
+    @ParameterizedTest
+    @EnumSource(TestCountry.class)
+    void shouldNotGetGymPasses_whenStartDateAfterEndDate(TestCountry country)
+            throws Exception {
+        Map<String, String> messages = getMessagesAccordingToLocale(country);
+        Locale testedLocale = convertEnumToLocale(country);
+
+        String purchaseStartDate = "2030-12-31";
+        String purchaseEndDate = "2000-01-01";
+        int page = 0;
+
+        URI uri = new URI("http://localhost:" + port + "/purchase/page/"+page+
+                "?purchaseStartDate="+purchaseStartDate+"&purchaseEndDate="+purchaseEndDate);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept-Language", testedLocale.toString());
+        headers.set("Authorization", employeeToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Object> request = new HttpEntity<>(null, headers);
+
+        ResponseEntity<JsonNode> responseEntity = restTemplate
+                .exchange(uri, HttpMethod.GET, request, JsonNode.class);
+
+        String expectedMessage = messages.get("exception.start.after.end");
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(responseEntity.getBody().get("status").intValue()).isEqualTo(400);
+        assertThat(responseEntity.getBody().get("error").textValue()).isEqualTo("Bad Request");
+        assertThat(responseEntity.getBody().get("message").textValue()).isEqualTo(expectedMessage);
+        assertThat(responseEntity.getBody().get("timestamp")).isNotNull();
+    }
+
     @Nested
     class ShouldNotGetGymPasses{
 

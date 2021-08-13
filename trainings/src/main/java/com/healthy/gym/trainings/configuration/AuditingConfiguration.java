71791eda1578eab5eacfaeca7a3213f970f230d6
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-08-13 14:11:56 +0200

    Remove duplicated bean from AuditingConfiguration.

--- trainings/src/main/java/com/healthy/gym/trainings/configuration/AuditingConfiguration.java
+++ trainings/src/main/java/com/healthy/gym/trainings/configuration/AuditingConfiguration.java
@@ -1,19 +1,9 @@
 package com.healthy.gym.trainings.configuration;
 
-import com.healthy.gym.trainings.component.UserAuditor;
-import com.healthy.gym.trainings.data.document.UserDocument;
-import com.healthy.gym.trainings.data.repository.UserDAO;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
 @EnableMongoAuditing
 public class AuditingConfiguration {
-
-    @Bean
-    public AuditorAware<UserDocument> getAuditorAwareProvider(UserDAO userDAO) {
-        return new UserAuditor(userDAO);
-    }
 }

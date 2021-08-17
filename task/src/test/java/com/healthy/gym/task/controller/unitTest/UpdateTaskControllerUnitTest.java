e63fa893ae8342a3d74b8a86944f0292f37c21a2
bartoszkordek <bartosz.kordek10@gmail.com>
2021-08-17 21:37:00 +0200

    shouldThrowRetroDueDateException controller unit test

--- task/src/test/java/com/healthy/gym/task/controller/unitTest/UpdateTaskControllerUnitTest.java
+++ task/src/test/java/com/healthy/gym/task/controller/unitTest/UpdateTaskControllerUnitTest.java
@@ -8,6 +8,7 @@ import com.healthy.gym.task.dto.TaskDTO;
 import com.healthy.gym.task.enums.AcceptanceStatus;
 import com.healthy.gym.task.exception.EmployeeNotFoundException;
 import com.healthy.gym.task.exception.ManagerNotFoundException;
+import com.healthy.gym.task.exception.RetroDueDateException;
 import com.healthy.gym.task.pojo.request.ManagerOrderRequest;
 import com.healthy.gym.task.service.TaskService;
 import org.junit.jupiter.api.BeforeEach;
@@ -403,6 +404,37 @@ public class UpdateTaskControllerUnitTest {
                     .andExpect(result ->
                             assertThat(Objects.requireNonNull(result.getResolvedException()).getCause())
                                     .isInstanceOf(EmployeeNotFoundException.class)
+                    );
+        }
+
+
+        @ParameterizedTest
+        @EnumSource(TestCountry.class)
+        void shouldThrowRetroDueDateException(TestCountry country) throws Exception {
+            Map<String, String> messages = getMessagesAccordingToLocale(country);
+            Locale testedLocale = convertEnumToLocale(country);
+
+            RequestBuilder request = MockMvcRequestBuilders
+                    .put(uri+taskId)
+                    .header("Accept-Language", testedLocale.toString())
+                    .header("Authorization", adminToken)
+                    .content(requestContent)
+                    .contentType(MediaType.APPLICATION_JSON);
+
+
+            String expectedMessage = messages.get("exception.retro.due.date");
+
+            doThrow(RetroDueDateException.class)
+                    .when(taskService)
+                    .updateTask(any(),any());
+
+            mockMvc.perform(request)
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(status().reason(is(expectedMessage)))
+                    .andExpect(result ->
+                            assertThat(Objects.requireNonNull(result.getResolvedException()).getCause())
+                                    .isInstanceOf(RetroDueDateException.class)
                     );
         }
     }

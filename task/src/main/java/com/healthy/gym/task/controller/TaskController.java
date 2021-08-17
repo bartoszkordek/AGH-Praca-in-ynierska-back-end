4b0288ff69c462347f5b004a9d582aacb3cf651a
bartoszkordek <bartosz.kordek10@gmail.com>
2021-08-17 20:18:36 +0200

    getOkStatus removed

--- task/src/main/java/com/healthy/gym/task/controller/TaskController.java
+++ task/src/main/java/com/healthy/gym/task/controller/TaskController.java
@@ -41,10 +41,6 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    @GetMapping
-    public String getOkStatus(){
-        return "OK";
-    }
 
     @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER')")
     @PostMapping

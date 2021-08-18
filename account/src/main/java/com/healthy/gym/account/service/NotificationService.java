5b24830b3a61995b9b817f38578d3e36e9264e3d
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-08-18 19:47:30 +0200

    Add NoNotificationFoundException.

--- account/src/main/java/com/healthy/gym/account/service/NotificationService.java
+++ account/src/main/java/com/healthy/gym/account/service/NotificationService.java
@@ -1,6 +1,7 @@
 package com.healthy.gym.account.service;
 
 import com.healthy.gym.account.dto.UserNotificationDTO;
+import com.healthy.gym.account.exception.NoNotificationFoundException;
 import com.healthy.gym.account.exception.NotificationNotFoundException;
 import com.healthy.gym.account.exception.UserNotFoundException;
 
@@ -8,7 +9,7 @@ import java.util.List;
 
 public interface NotificationService {
     List<UserNotificationDTO> getRecentUserNotifications(String userId, int pageNumber, int pageSize)
-            throws UserNotFoundException;
+            throws NoNotificationFoundException, UserNotFoundException;
 
     UserNotificationDTO markNotificationAsRead(String notificationId, String userId)
             throws NotificationNotFoundException, UserNotFoundException;

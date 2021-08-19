05e89db04a3711f0855b4bb4a4c0abb97d26a6f1
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-08-19 22:20:24 +0200

    Add setup script

---
+++ app_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd
+mkdir -p app/front-end
+mkdir -p app/back-end/properties
+
+curl -v -X GET https://raw.githubusercontent.com/bartoszkordek/AGH-Praca-inzynierska-back-end/main/docker-compose-front.yaml > ~/app/front-end/docker-compose-front.yaml
+curl -v -X GET https://raw.githubusercontent.com/bartoszkordek/AGH-Praca-inzynierska-back-end/main/docker-compose.yaml > ~/app/back-end/docker-compose.yaml
+curl -v -X GET https://raw.githubusercontent.com/bartoszkordek/AGH-Praca-inzynierska-back-end/main/properties/application-example.properties > ~/app/back-end/properties/application.properties

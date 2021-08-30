ca448dd5368251014c5f1b1b456507c2b90e3f84
bartoszkordek <bartosz.kordek10@gmail.com>
2021-08-31 00:56:16 +0200

    added Photo controller and service

---
+++ equipment/src/main/java/com/healthy/gym/equipment/controller/PhotoController.java
@@ -0,0 +1,56 @@
+package com.healthy.gym.equipment.controller;
+
+import com.healthy.gym.equipment.component.Translator;
+import com.healthy.gym.equipment.exception.ImageNotFoundException;
+import com.healthy.gym.equipment.service.PhotoService;
+import org.springframework.http.*;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.Duration;
+
+@RestController
+public class PhotoController {
+
+    private final Translator translator;
+    private final PhotoService photoService;
+
+    public PhotoController(Translator translator, PhotoService photoService) {
+        this.translator = translator;
+        this.photoService = photoService;
+    }
+
+    @GetMapping("/trainingType/image/{imageId}")
+    public ResponseEntity<byte[]> getAvatar(@PathVariable String imageId) {
+        try {
+            byte[] image = photoService.getImage(imageId);
+            String etag = DigestUtils.md5DigestAsHex(image);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentLength(image.length);
+            headers.setContentType(MediaType.IMAGE_JPEG);
+
+            CacheControl cacheControl = CacheControl.maxAge(Duration.ofMinutes(10));
+            cacheControl.cachePrivate();
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .headers(headers)
+                    .cacheControl(cacheControl)
+                    .eTag(etag)
+                    .body(image);
+
+        } catch (ImageNotFoundException exception) {
+            String reason = translator.toLocale("avatar.not.found.exception");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, reason, exception);
+
+        } catch (Exception exception) {
+            String reason = translator.toLocale("exception.internal.error");
+            exception.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, reason, exception);
+        }
+    }
+}
